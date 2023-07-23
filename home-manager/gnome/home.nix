b10ee13b1219a849532b0b6e7d8c6ec0cbe0f305
akibahmed229 <akib4418@gmail.com>
2023-07-23 11:08:42 +0600

    Desktop Environment organized

--- home-manager/gnome/home.nix
+++ home-manager/gnome/home.nix
@@ -115,7 +115,7 @@
       ];
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      binding = "<Ctrl><Alt>t";
+      binding = "<Super>Return";
       command = "alacritty";
       name = "open-terminal";
     };
