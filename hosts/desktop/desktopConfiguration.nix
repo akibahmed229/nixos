6c781de2999992c033d82fc080750ff0a5011a6e
akibahmed229 <akib4418@gmail.com>
2023-07-27 16:42:53 +0600

    Bloat remove

--- hosts/desktop/desktopConfiguration.nix
+++ hosts/desktop/desktopConfiguration.nix
@@ -25,6 +25,8 @@
   # remove bloat
   documentation.nixos.enable = false;
 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
