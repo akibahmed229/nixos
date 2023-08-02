767ed3c2b30fec6062ff57a0bfdf03b2016759c1
akibahmed229 <akib4418@gmail.com>
2023-08-02 23:23:38 +0600

    Flatpk config added

---
+++ programs/flatpak/flatpak.nix
@@ -0,0 +1,69 @@
+#
+# Very janky way of declaring all flatpaks used
+# Might cause issues on new system installs
+# Only use when you know what you're doing
+#
+
+{ pkgs, ...}:
+
+{
+  services.flatpak.enable = true;
+  system.activationScripts = {
+    flatpak.text =
+      ''
+        flatpaks=(
+                  com.getpostman.Postman
+                  com.github.flxzt.rnote
+                  com.github.tchx84.Flatseal
+                  Chrome
+                  com.leinardi.gst
+                  Manager
+                  Edge
+                  io.github.amit9838.weather
+                  io.github.jonmagon.kdiskmark
+                  ir.imansalmani.IPlan
+                  md.obsidian.Obsidian
+                  Platform
+                  org.freedesktop.Platform.GL.default
+                  org.freedesktop.Platform.GL.default
+                  (Extra)
+                  org.freedesktop.Platform.VAAPI.Intel
+                  org.freedesktop.Platform.VAAPI.Intel
+                  org.freedesktop.Platform.openh264
+                  org.freedesktop.Platform.openh264
+                  org.gnome.Logs
+                  Application
+                  Application
+                  org.gnome.Snapshot
+                  Application
+                  Application
+                  org.kde.PlatformTheme.QGnomePlatform
+                  org.kde.PlatformTheme.QGnomePlatform
+                  org.kde.PlatformTheme.QtSNI
+                  org.kde.WaylandDecoration.QGnomePlatform-decoration
+                  org.kde.WaylandDecoration.QGnomePlatform-decoration
+                  org.mozilla.firefox
+                  org.qbittorrent.qBittorrent
+                  org.signal.Signal  
+        )
+
+        ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+        for package in ''${flatpaks[*]}; do
+          check=$(${pkgs.flatpak}/bin/flatpak list --app | ${pkgs.gnugrep}/bin/grep $package)
+          if [[ -z "$check" ]] then
+            ${pkgs.flatpak}/bin/flatpak install -y flathub $package
+          fi
+        done
+
+        installed=($(${pkgs.flatpak}/bin/flatpak list --app | ${pkgs.gawk}/bin/awk -F$'\t*' '{$1=$3=$4=$5=""; print $0}'))
+
+        for remove in ''${installed[*]}; do
+          if [[ ! " ''${flatpaks[*]} " =~ " ''${remove} " ]]; then
+            ${pkgs.flatpak}/bin/flatpak uninstall -y $remove
+            ${pkgs.flatpak}/bin/flatpak uninstall -y --unused
+          fi
+        done
+      '';
+  };
+}
