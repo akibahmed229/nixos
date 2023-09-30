8f46ad5d1bf7ea9153bac7d4340de534fbfbe14e
akibahmed229 <akib4418@gmail.com>
2023-09-30 14:30:12 +0600

    discord overlay added to pull latest

--- hosts/desktop/desktopConfiguration.nix
+++ hosts/desktop/desktopConfiguration.nix
@@ -174,6 +174,18 @@
   };
   hardware.opengl.driSupport32Bit = true;
 
+  # Overlay pulls latest version of Discord
+  nixpkgs.overlays = [                          
+      (self: super: {
+        discord = super.discord.overrideAttrs (
+          _: { src = builtins.fetchTarball {
+            url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+            sha256 = "0yzgkzb0200w9qigigmb84q4icnpm2hj4jg400payz7igxk95kqk";
+          };}
+        );
+      })
+  ];
+
 # Zsh and Oh-My-Zsh setup
   programs = {
     zsh = {
