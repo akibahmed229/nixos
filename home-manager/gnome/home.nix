fede2154354bdd1ecfe633619497af055bd101da
akibahmed229 <akib4418@gmail.com>
2023-10-19 02:47:53 +0600

    theme mechanism added for dynamicially updte

--- home-manager/gnome/home.nix
+++ home-manager/gnome/home.nix
@@ -243,11 +243,11 @@
 
   home.file = {
       ".config/gtk-4.0" = {
-        source = $../../themes/gtk/${theme};
+        source = ../../themes/gtk/${theme};
         recursive = true;
     };
     ".config/gtk-3.0" = {
-      source = $../../themes/gtk/${theme};
+      source = ../../themes/gtk/${theme};
       recursive = true;
     };
   };
