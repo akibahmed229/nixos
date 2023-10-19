55945cf11ca5d345d6752b269e658117557dcddf
akibahmed229 <akib4418@gmail.com>
2023-10-19 16:43:05 +0600

    new host virt manager added

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,30 @@
 { lib, inputs, unstable, system, theme, home-manager, hyprland, plasma-manager, user, ... }:
 
 {
+# Host desktop configuration
   desktop = lib.nixosSystem {
+    inherit system;
+    specialArgs = { inherit user inputs unstable; };
+    modules = [
+      ./configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit user inputs theme; };
+          home-manager.users.${user} = {
+            imports = [
+              # inputs.plasma-manager.homeManagerModules.plasma-manager  # uncommnet to use KDE Plasma 
+              ../home-manager/home.nix
+            ];
+          };
+        }
+    ];
+  };
+  
+# Host virt-managet configuration
+
+  virt = lib.nixosSystem {
     inherit system;
     specialArgs = { inherit user inputs unstable; };
     modules = [
