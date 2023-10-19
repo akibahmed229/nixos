33d7882633f9bd92b5a331066eb553eb965efe27
akibahmed229 <akib4418@gmail.com>
2023-10-19 18:32:17 +0600

    forefox declearation added

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, user, ... }:
+{ config, pkgs, user, state-version, ... }:
 
 {
   imports =   
@@ -20,7 +20,7 @@
 # You should not change this value, even if you update Home Manager. If you do
 # want to update the value, then make sure to first check the Home Manager
 # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "${state-version}"; # Please read the comment before changing.
 
 # The home.packages option allows you to install Nix packages into your
 # environment.
