cf489df1628a730aad30f2a3e1ee91091740c6da
akibahmed229 <akib4418@gmail.com>
2023-10-21 22:00:41 +0600

    flake refactore for system

--- flake.nix
+++ flake.nix
@@ -15,43 +15,50 @@
   description = "My NixOS configuration";
 
 # inputs for the flake
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.05"; # stable packages
+  inputs = { 
+    # stable packages
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.05";
+    # unstable packages 
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable"; # unstable packages 
-
+    # flake-programs-sqlite makes use of a mapping from git commit hashes to channel names.
     programsdb = {
        url = "github:wamserma/flake-programs-sqlite";
        inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    
+    # nix-index is a tool to quickly locate the package providing a certain file in nixpkgs. It indexes built derivations found in binary caches.
     nix-index-database = { 
       url = "github:Mic92/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   
+    # stable home-manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05"; # stable home-manager
+      url = "github:nix-community/home-manager/release-23.05"; 
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+    # unstable home-manager
     home-manager-unstable = {
-      url = "github:nix-community/home-manager/master"; # unstable home-manager
+      url = "github:nix-community/home-manager/master"; 
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Add "hyprland.nixosModules.default" to the host moduls list in ./hosts/default.nix
     hyprland = {
-      url = "github:hyprwm/Hyprland"; # Add "hyprland.nixosModules.default" to the host moduls list in ./hosts/default.nix
+      url = "github:hyprwm/Hyprland"; 
       inputs.nixpkgs.follows = "nixpkgs";
     };  
 
-    plasma-manager = {  # KDE Plasma user settings
-      url = "github:pjones/plasma-manager"; # Add "inputs.plasma-manager.homeManagerModules.plasma-manager" to the home-manager.users.${user}.imports list in ./hosts/default.nix
+    # Add "inputs.plasma-manager.homeManagerModules.plasma-manager" to the home-manager.users.${user}.imports list in ./hosts/default.nix
+    plasma-manager = {  
+      url = "github:pjones/plasma-manager"; 
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "nixpkgs";
     };
 
     
+    # firefox-addons is a collection of Firefox extensions
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -63,10 +70,12 @@
 
 # variables
     let
+      # The system to build.
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       state-version = "23.05";
-
+      
+      # The user to build for.
       user = "akib";
       theme = "gruvbox-dark-soft";
 
@@ -84,7 +93,7 @@
   in {
 
 # NixOS configuration
-    nixosConfigurations = ( # NixOS configurations
+    nixosConfigurations = ( 
         import ./hosts {    # Imports ./hosts/default.nix
         inherit (nixpkgs) lib;
         inherit inputs unstable user system theme state-version nix-index-database home-manager hyprland plasma-manager;   # Also inherit home-manager so it does not need to be defined here.
