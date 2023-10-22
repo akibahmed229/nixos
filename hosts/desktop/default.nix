ddd744fce2d0ba01ee0427e2debb6096df6d0271
akibahmed229 <akib4418@gmail.com>
2023-10-22 15:04:30 +0600

    some changes here and there

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -33,7 +33,7 @@
 # List packages installed in system profile. To search, run:
 # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim
+      neovim-unwrapped
       htop
       trash-cli
       cava
