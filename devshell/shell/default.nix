7aaf36f6e18db5cf12effcf194afa6eb0608b2d2
akibahmed229 <akib4418@gmail.com>
2023-10-23 19:50:24 +0600

    gtk3 dev environment defined

--- devshell/shell/default.nix
+++ devshell/shell/default.nix
@@ -6,4 +6,6 @@
   python = import ./python/python.nix { inherit system pkgs inputs; };
 
   nodejs = import ./nodejs/nodejs.nix { inherit pkgs; };
+
+  gtk3_env = import ./c_c++/gtk3_env.nix {inherit pkgs; };
 }
