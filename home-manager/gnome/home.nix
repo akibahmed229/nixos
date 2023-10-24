cfef0ca38bc54a678cf10a7f1322cc3d035bfa26
akibahmed229 <akib4418@gmail.com>
2023-10-25 02:06:16 +0600

    dconf setting changed dash-to-panel && dash-to-dock

--- home-manager/gnome/home.nix
+++ home-manager/gnome/home.nix
@@ -109,7 +109,7 @@
     };
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"gnome
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
       ];
