525ee17615f17bc084943fd829cbd7818c0b2cf4
akibahmed229 <akib4418@gmail.com>
2023-10-25 15:32:03 +0600

    some package added

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -26,6 +26,8 @@
 # The home.packages option allows you to install Nix packages into your
 # environment.
     home.packages = with pkgs; [
+    	xdg-utils
+	    wl-clipboard
       tree
       bibata-cursors
       adw-gtk3
