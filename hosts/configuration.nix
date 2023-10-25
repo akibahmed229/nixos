d0ff56357ce42745d84efb2fe049743890a4b838
akibahmed229 <akib4418@gmail.com>
2023-10-25 15:32:30 +0600

    xdg path added

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -108,4 +108,18 @@
       };
     };
   };
+
+# XDG  paths
+    environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [ 
+      "${XDG_BIN_HOME}"
+    ];
+  };
 }
