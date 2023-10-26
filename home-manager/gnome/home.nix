116d5cb4aa08f3e7ff8ff330426d77f3d403a22b
akibahmed229 <akib4418@gmail.com>
2023-10-26 19:13:31 +0600

    refactore and added some dconf setting for pereferd gnome-extension

--- home-manager/gnome/home.nix
+++ home-manager/gnome/home.nix
@@ -302,28 +302,25 @@
     };
   };
 
-  home.packages = with unstable; [
+  home.packages = with pkgs; [
 # gnomeExtensions.tray-icons-reloaded
-      gnomeExtensions.forge
-      gnomeExtensions.space-bar
+      unstable.gnomeExtensions.forge
+      unstable.gnomeExtensions.space-bar
       unstable.gnomeExtensions.removable-drive-menu
-      gnomeExtensions.clipboard-indicator
-      gnomeExtensions.blur-my-shell
-      gnomeExtensions.burn-my-windows
-      gnomeExtensions.caffeine
-      gnomeExtensions.custom-hot-corners-extended
-      gnomeExtensions.clipboard-indicator
+      unstable.gnomeExtensions.blur-my-shell
+      unstable.gnomeExtensions.burn-my-windows
+      unstable.gnomeExtensions.caffeine
+      unstable.gnomeExtensions.custom-hot-corners-extended
+      unstable.gnomeExtensions.clipboard-indicator
       gnomeExtensions.coverflow-alt-tab
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.dash-to-panel
-      gnomeExtensions.user-avatar-in-quick-settings
-      gnomeExtensions.gnome-40-ui-improvements
-      gnomeExtensions.gsconnect
+      unstable.gnomeExtensions.dash-to-dock
+      unstable.gnomeExtensions.dash-to-panel
+      unstable.gnomeExtensions.user-avatar-in-quick-settings
+      unstable.gnomeExtensions.gnome-40-ui-improvements
+      unstable.gnomeExtensions.gsconnect
       gnomeExtensions.impatience
       gnomeExtensions.quick-settings-tweaker
-      gnomeExtensions.tiling-assistant
       gnomeExtensions.vitals
-      gnomeExtensions.pop-shell
       gnomeExtensions.rounded-window-corners
       gnomeExtensions.just-perfection
       gnomeExtensions.advanced-alttab-window-switcher
