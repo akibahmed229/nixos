ac7ccc523d3d6e303c27f77da30659834f52b4ee
akibahmed229 <akib4418@gmail.com>
2023-10-31 02:47:00 +0600

    hyparland added as desktop environment

--- hosts/virt/default.nix
+++ hosts/virt/default.nix
@@ -17,6 +17,11 @@
   #  wget
   ];
 
+  programs = {
+    zsh = {
+      enable = true;
+    }
+  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
