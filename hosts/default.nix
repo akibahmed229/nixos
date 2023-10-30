571e662c8d9cc5953e2098422c009e1409843726
akibahmed229 <akib4418@gmail.com>
2023-10-30 22:35:19 +0600

    some changes regarding hayprland and virto

--- hosts/default.nix
+++ hosts/default.nix
@@ -22,11 +22,11 @@
             imports = [
               # inputs.plasma-manager.homeManagerModules.plasma-manager  # uncommnet to use KDE Plasma 
               ../home-manager/home.nix
+              ../home-manager/gnome/home.nix
               nix-index-database.hmModules.nix-index
               # optional to also wrap and install comma
               # { programs.nix-index-database.comma.enable = true; }
               # hyprland.homeManagerModules.default # uncommnet to use hyprland
-              # {wayland.windowManager.hyprland.enable = true;} # uncommnet to use hyprland
             ];
           };
         }
@@ -48,7 +48,9 @@
           home-manager.users.${user} = {
             imports = [
               # inputs.plasma-manager.homeManagerModules.plasma-manager  # uncommnet to use KDE Plasma 
+              hyprland.homeManagerModules.default # uncommnet to use hyprland
               ../home-manager/home.nix
+              ../home-manager/hyprland/home.nix
             ];
           };
         }
