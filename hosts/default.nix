6f0a4c577c2bfe2e6facb8a8563fabe2faacbd3b
akibahmed229 <akib4418@gmail.com>
2023-10-31 22:48:36 +0600

    some changes regarding hayprland in virt conf

--- hosts/default.nix
+++ hosts/default.nix
@@ -48,6 +48,7 @@
           home-manager.users.${user} = {
             imports = [
               # inputs.plasma-manager.homeManagerModules.plasma-manager  # uncommnet to use KDE Plasma 
+              hyprland.homeManagerModules.default # uncommnet to use hyprland
               ../home-manager/home.nix
               ../home-manager/hyprland/home.nix
             ];
