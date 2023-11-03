b7b64294677d1a918f3300f15c65ad60a9036579
akibahmed229 <akib4418@gmail.com>
2023-11-04 02:32:00 +0600

    some changes on tmux

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -125,8 +125,16 @@
         tmuxPlugins.vim-tmux-navigator
         tmuxPlugins.yank
         tmuxPlugins.tmux-fzf
-        tmuxPlugins.resurrect
-        tmuxPlugins.continuum
+    {
+      plugin = tmuxPlugins.resurrect;
+      extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+    }
+    {
+      plugin = tmuxPlugins.continuum;
+      extraConfig = ''
+        set -g @continuum-restore 'on'
+        set -g @continuum-save-interval '60' # minutes
+        '';
     ];
     extraConfig = ''	
       source /home/${user}/flake/programs/tmux/tmux.conf
