3ed1251a1408f9992ab7c914561e214a1863cf74
akibahmed229 <akib4418@gmail.com>
2023-12-26 12:54:47 +0600

    zram algorithm added to lz4

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -115,6 +115,7 @@
   zramSwap = {
     enable = true;
     memoryPercent = 20;
+    algorithm = "lz4";
   };
 
 # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
