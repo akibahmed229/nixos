f12ced1b116363233de9ee8c8c750701794ce4ac
Akib Ahmed <49590623+akibahmed229@users.noreply.github.com>
2024-01-01 20:58:32 +0600

    Update discord.nix

--- programs/discord/discord.nix
+++ programs/discord/discord.nix
@@ -16,15 +16,15 @@
   ];
 
 # Overlay pulls latest version of Discord
-  nixpkgs.overlays = [                          
-    (self: super: {
-     discord = super.discord.overrideAttrs (
-         _: { src = builtins.fetchTarball {
-         url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-         sha256 = "0yzgkzb0200w9qigigmb84q4icnpm2hj4jg400payz7igxk95kqk";
-         };
-         }
-         );
-     })
-  ];
-}
+#  nixpkgs.overlays = [                          
+ #   (self: super: {
+#     discord = super.discord.overrideAttrs (
+#         _: { src = builtins.fetchTarball {
+#         url = #"https://discord.com/api/download?platform=linux&format=tar.gz";
+     #    sha256 = "0yzgkzb0200w9qigigmb84q4icnpm2hj4jg400payz7igxk95kqk";
+ #        };
+#         }
+ #        );
+#     })
+#  ];
+#}
