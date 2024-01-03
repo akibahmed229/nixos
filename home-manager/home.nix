eaab3ea76674951faec074fb8708ec1fcf93cbd5
akibahmed229 <akib4418@gmail.com>
2024-01-03 17:47:11 +0600

    qt theme added

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -98,6 +98,25 @@
     };
   };
 
+qt = {
+  enable = true;
+
+  # platform theme "gtk" or "gnome"
+  platformTheme = "gtk";
+  
+  # name of the qt theme
+  style.name = "adwaita-dark";
+  
+  # detected automatically:
+  # adwaita, adwaita-dark, adwaita-highcontrast,
+  # adwaita-highcontrastinverse, breeze,
+  # bb10bright, bb10dark, cde, cleanlooks,
+  # gtk2, motif, plastique
+  
+  # package to use
+  style.package = pkgs.adwaita-qt;
+};
+
 # You can also manage environment variables but you will have to manually
 # source
 #
