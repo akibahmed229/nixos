0904d7d94701c717d06d54a1dc9941dba3288149
akibahmed229 <akib4418@gmail.com>
2024-01-04 18:16:13 +0600

    some change regarding window resizing

--- home-manager/hyprland/home.nix
+++ home-manager/hyprland/home.nix
@@ -81,6 +81,10 @@
       "$mod SHIFT, left, resizeactive, -100 0"
       "$mod SHIFT, up, resizeactive, 0 -100"
       "$mod SHIFT, down, resizeactive, 0 100"
+      "$mod ALT, right, movewindow, r"
+      "$mod ALT, left, movewindow, l"
+      "$mod ALT, up, movewindow, u"
+      "$mod ALT, down, movewindow, d"
 
       ]
         ++ (
