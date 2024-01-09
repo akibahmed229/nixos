092a3da5c8d97120861825e4a0e0731bb2e16ffb
akibahmed229 <akib4418@gmail.com>
2024-01-09 20:54:49 +0600

    some changes on system and code refactore

--- home-manager/hyprland/others/hyprutility.nix
+++ home-manager/hyprland/others/hyprutility.nix
@@ -7,7 +7,7 @@ theme, ... }:
 
   programs.waybar = {
     enable = true;
-    settings = [ builtins.readFile ./waybar/config ] ;
+    settings =  builtins.readFile ./waybar/config ;
     style = builtins.readFile ./waybar/style.css;
   };
   
@@ -18,7 +18,7 @@ theme, ... }:
 
   programs.wlogout = {
     enable = true;
-    layout = [builtins.readFile ./wlogout/layout];
+    layout = builtins.readFile ./wlogout/layout;
     style =  builtins.readFile ./wlogout/style.css;
   };
 
