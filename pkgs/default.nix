ede11658131d4fb50c93d59fa9a2f7d975d12457
akibahmed229 <akib4418@gmail.com>
2024-01-16 15:28:10 +0600

    module seperated

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,10 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 pkgs: {
-  nvchad = pkgs.callPackage ./nvchad/default.nix { };
+  nvchad = pkgs.callPackage ./nvchad { };
 
   custom_sddm = pkgs.callPackage ./sddm/sddm.nix { };
 
-  nix-update-input = pkgs.callPackage ./nix-update-input/nix-update-input.nix { };
+  nix-update-input = pkgs.callPackage ./nix-update-input { };
 
 }
