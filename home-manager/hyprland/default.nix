9e3911876128d8ade555112601adc2fc9a12dc3d
akibahmed229 <akib4418@gmail.com>
2024-01-20 13:45:48 +0600

    kde portal removed

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -134,7 +134,7 @@
     extraPortals = with pkgs;[
       xdg-desktop-portal-gtk
       #xdg-desktop-portal-wlr
-      xdg-desktop-portal-kde
+      #xdg-desktop-portal-kde
       #xdg-desktop-portal-hyprland
     ];
   };
