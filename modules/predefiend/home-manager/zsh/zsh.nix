ce34851b24de591f385d5e18edb6238cef5b2234
akibahmed229 <akib4418@gmail.com>
2024-01-29 18:23:16 +0600

    changes: zsh settings

--- modules/predefiend/home-manager/zsh/zsh.nix
+++ modules/predefiend/home-manager/zsh/zsh.nix
@@ -9,16 +9,39 @@
     zsh = {
       enable = true;
       enableAutosuggestions = true;
+      envExtra = ''
+                        colorscript random
+                        setopt hist_ignore_all_dups     # Ignore duplicate commands in history
+                        setopt hist_ignore_space        # Ignore commands starting with a space in h>
+                        setopt hist_reduce_blanks       # Remove redundant blanks from history
+                        setopt hist_verify              # Verify history expansions before execution
+                        # setopt
+                        setopt extended_glob            #  control over file matching
+                        setopt autocd                   # enable auto cd
+                        setopt correct                  # Enable auto-correction
+                        # alias
+                        alias ll='ls -l'
+                        alias cat='bat'
+                        alias ga='git add'
+                        alias gc='git commit'
+                        alias gs='git status'
+                        alias gd='git diff'
+                        alias ..='cd ..'
+        		            #alias vim='nvim'
+                        # history search
+                        bindkey "^[[A" history-beginning-search-backward
+                        bindkey "^[[B" history-beginning-search-forward
+                        bindkey 'Ctrl-g' kill-line
+                        bindkey 'Ctrl-A' beginning-of-line
+                        bindkey 'Ctrl-E' end-of-line
+      '';
       shellAliases = {
         la = "eza --icons -la  --group-directories-first";
         ls = "eza --icons --grid --group-directories-first";
       };
       syntaxHighlighting.enable = true;
       enableCompletion = true;
-      # shellInit = ''
-      # '';
       initExtra = ''
-        source /home/${user}/flake/modules/predefiend/home-manager/zsh/.zshrc
         export LANG="en_US.UTF-8";
         function tmux-sesssion {
         BUFFER='tmux-sessionizer'
