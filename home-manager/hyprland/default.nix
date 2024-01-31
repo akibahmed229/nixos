e2e0c916119ebbe2b2dc33630d9f40db0fd5360f
akibahmed229 <akib4418@gmail.com>
2024-01-31 23:19:18 +0600

    BREAKING CHANGE: modularized sddm theme by adding override option from pkgs

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -35,7 +35,11 @@
             background = "/home/${user}/.cache/swww/Wallpaper";
           };
         };
-        sddm.theme = "${pkgs.callPackage ../../pkgs/sddm/sddm.nix { }}";
+        sddm.theme = ''${pkgs.callPackage ../../pkgs/sddm/sddm.nix { imgLink = {
+          url = "https://raw.githubusercontent.com/akibahmed229/nixos/main/public/wallpaper/nixos.png"; 
+          sha256 = "sha256-QcY0x7pE8pKQy3At81/OFl+3CUAbx0K99ZHk85QLSo0=";
+         };
+        }}'';
       };
     };
   };
--- pkgs/sddm/sddm.nix
+++ pkgs/sddm/sddm.nix
@@ -1,12 +1,22 @@
-{ pkgs }:
+{ pkgs
+, imgLink ? { url = "https://raw.githubusercontent.com/akibahmed229/nixos/main/public/wallpaper/Photo-of-Valley.jpg"; sha256 = "sha256-86ja7yfSdMAaFFdfDqQS8Yq5yjZjXPMLTIgExcnzbXc="; }
+, ...
+}:
 
 let
-  imgLink = "https://raw.githubusercontent.com/akibahmed229/nixos/main/public/wallpaper/Photo-of-Valley.jpg";
-
-  image = pkgs.fetchurl {
-    url = imgLink;
-    sha256 = "sha256-86ja7yfSdMAaFFdfDqQS8Yq5yjZjXPMLTIgExcnzbXc=";
-  };
+  image =
+    if imgLink.url == "https://raw.githubusercontent.com/akibahmed229/nixos/main/public/wallpaper/Photo-of-Valley.jpg" then
+      pkgs.fetchurl
+        {
+          url = imgLink.url;
+          sha256 = imgLink.sha256;
+        }
+    else
+      pkgs.fetchurl
+        {
+          url = imgLink.url;
+          sha256 = imgLink.sha256;
+        };
 in
 pkgs.stdenv.mkDerivation {
   name = "sddm-theme";
@@ -16,11 +26,21 @@ pkgs.stdenv.mkDerivation {
     rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
     sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
   };
+
   installPhase = ''
     mkdir -p $out
     cp -R ./* $out/
     cd $out/
     rm Background.jpg
-    cp -r ${image} $out/Background.jpg
+    cp -r ${image} new.jpg
+    cp -r new.jpg $out/Background.jpg
   '';
+
+  meta = with pkgs.lib; {
+    description = "A dark theme for SDDM";
+    homepage = "https://github.com/MarianArlt/sddm-sugar-dark";
+    platforms = platforms.linux;
+    maintainers = [ maintainers.akibahmed229 ];
+    license = licenses.gpl3;
+  };
 }
