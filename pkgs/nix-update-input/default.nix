ba4a6e856a62a7cf9e02f6a174c8ab24daa15749
akibahmed229 <akib4418@gmail.com>
2024-02-04 20:25:18 +0600

    feat: code refactor

--- pkgs/nix-update-input/default.nix
+++ pkgs/nix-update-input/default.nix
@@ -3,9 +3,9 @@
 # sudo nix run <path/to/flake.nix>/ # to update the input of a flake
 pkgs.writeShellScriptBin "update-input" ''
   input=$(                                           \
-    nix flake metadata --json                        \
-    | ${pkgs.jq}/bin/jq ".locks.nodes.root.inputs[]" \
-    | sed "s/\"//g"                                  \
-    | ${pkgs.fzf}/bin/fzf)
-  nix flake lock --update-input $input
+            nix flake metadata --json                        \
+            | ${pkgs.jq}/bin/jq ".locks.nodes.root.inputs | keys[]" \
+            | sed "s/\"//g"                                  \
+            | ${pkgs.fzf}/bin/fzf)
+          nix flake lock --update-input $input
 ''
