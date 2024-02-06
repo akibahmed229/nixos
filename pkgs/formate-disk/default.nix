b2942f6216de13040e22b519e490c66a011c4d77
akibahmed229 <akib4418@gmail.com>
2024-02-07 03:30:51 +0600

    test: imperance and disko by virt

--- pkgs/formate-disk/default.nix
+++ pkgs/formate-disk/default.nix
@@ -4,6 +4,6 @@ pkgs.writeShellScriptBin "disko-formate" ''
   echo "Enter device (e.g., /dev/vda): "
   read device
   curl https://raw.githubusercontent.com/akibahmed229/nixos/main/modules/predefiend/nixos/disko/disko.nix -o /tmp/disko.nix
-  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device $device
+  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device "${device}"
 ''
 
