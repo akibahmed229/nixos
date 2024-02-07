04e3043c50850bf37b83d10dbadb3e3aac1644f6
akibahmed229 <akib4418@gmail.com>
2024-02-08 01:53:26 +0600

    feat: persistant directory added

--- modules/predefiend/home-manager/impermanence/impermanence.nix
+++ modules/predefiend/home-manager/impermanence/impermanence.nix
@@ -5,7 +5,7 @@
     inputs.impermanence.nixosModules.home-manager.impermanence
   ];
 
-  home.persistence."/persist/home/${user}" = {
+  home.persistence."/persist/home" = {
     directories = [
       "Downloads"
       "Music"
