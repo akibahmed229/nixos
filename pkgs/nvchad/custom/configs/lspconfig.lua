09dff8a5f210c63127c4e24b9dee91c6a2401b13
akibahmed229 <akib4418@gmail.com>
2024-02-08 09:33:25 +0600

    feat: added new config

--- pkgs/nvchad/custom/configs/lspconfig.lua
+++ pkgs/nvchad/custom/configs/lspconfig.lua
@@ -54,6 +54,12 @@ lspconfig.clangd.setup {
   capabilities = capabilities,
 }
  
--- lspconfig.pyright.setup { blabla}
-
-
+-- if you just want default config for the servers then put them in a table
+--
+local servers = { "html", "cssls", "tailwindcss", ... }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
--- pkgs/nvchad/custom/init.lua
+++ pkgs/nvchad/custom/init.lua
@@ -5,3 +5,10 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
+
