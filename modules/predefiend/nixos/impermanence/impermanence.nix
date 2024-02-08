7c74860733a372fab6b4fb65c28fbdda42990543
akibahmed229 <akib4418@gmail.com>
2024-02-08 23:29:56 +0600

    feat: chaneges regarfing disko mount point

--- modules/predefiend/nixos/impermanence/impermanence.nix
+++ modules/predefiend/nixos/impermanence/impermanence.nix
@@ -27,30 +27,29 @@
   '';
 
   fileSystems."/persist".neededForBoot = true;
-  fileSystems."/persist/system".neededForBoot = true;
-  environment.persistence."/persist/system" = {
-    hideMounts = true;
-    directories = [
-      "/etc/nixos"
-      "/home"
-      "/var/log"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
-    ];
-    files = [
-      "/etc/machine-id"
-      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-    ];
-  };
-  fileSystems."/persist/home".neededForBoot = true;
-  environment.persistence."/persist/home" = {
-    hideMounts = true;
-    directories = [
-      "/home"
-    ];
+  environment.persistence = {
+    "/persist/system" = {
+      hideMounts = true;
+      directories = [
+        "/etc/nixos"
+        "/var/log"
+        "/var/lib/bluetooth"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
+        { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+      ];
+      files = [
+        "/etc/machine-id"
+        { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+      ];
+    };
+    "/persist/home" = {
+      hideMounts = true;
+      directories = [
+        "/home"
+      ];
+    };
   };
 
   programs.fuse.userAllowOther = true;
