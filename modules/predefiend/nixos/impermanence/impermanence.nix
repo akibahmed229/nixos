9a9e0eb5345d66129a334e84ad27a6d18af2bbea
akibahmed229 <akib4418@gmail.com>
2024-02-09 11:48:49 +0600

    feat: chaneges persistance file system

--- modules/predefiend/nixos/impermanence/impermanence.nix
+++ modules/predefiend/nixos/impermanence/impermanence.nix
@@ -34,6 +34,7 @@
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
+      "/etc/shadow"
       "/etc/NetworkManager/system-connections"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
