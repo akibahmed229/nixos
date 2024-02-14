399c3a315ef7fe68b0e2e433f3b2c46b2e2212db
akibahmed229 <akib4418@gmail.com>
2024-02-15 00:26:54 +0600

    feat: little changes

--- pkgs/shellscript/disko-formate.nix
+++ pkgs/shellscript/disko-formate.nix
@@ -3,7 +3,7 @@
 pkgs.writeShellScriptBin "disko-formate" ''
   echo "---------------------"
   echo "### Disko Formate ###"
-  echo "Enter device (e.g., /dev/vda): "
+  echo "Enter device (e.g., /dev/nvme0n1): "
   read device
   curl https://raw.githubusercontent.com/akibahmed229/nixos/dev/modules/predefiend/nixos/disko/disko.nix -o /tmp/disko.nix
   sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device "\"$device\""
