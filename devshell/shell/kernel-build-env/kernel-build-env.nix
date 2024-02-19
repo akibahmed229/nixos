8a336ed024002b8880b8b4632ea4cf18173c9c7d
akibahmed229 <akib4418@gmail.com>
2024-02-19 13:28:32 +0600

    feat: kernel-build-env added

--- devshell/shell/kernel-build-env/kernel-build-env.nix
+++ devshell/shell/kernel-build-env/kernel-build-env.nix
@@ -19,6 +19,6 @@
     export PKG_CONFIG_PATH="${pkgs.ncurses.dev}/lib/pkgconfig:${pkgs.qt5.qtbase.dev}/lib/pkgconfig"
     export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins"
     export QT_QPA_PLATFORMTHEME=qt5ct
-    exec bash
+    exec "${pkgs.zsh}/bin/zsh"
   '';
 }).env
