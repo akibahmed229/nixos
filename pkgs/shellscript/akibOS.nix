a277235a253ca6df5e24451458b28a611ca192c0
akibahmed229 <akib4418@gmail.com>
2024-02-29 01:06:28 +0600

    fix: single command to install flake

---
+++ pkgs/shellscript/akibOS.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.writeShellScriptBin "akibOS" ''
+  echo "---------------------"
+  echo "### Disko Formate ###"
+  echo "Enter device (e.g., /dev/nvme0n1): "
+  read device
+  curl https://raw.githubusercontent.com/akibahmed229/nixos/main/modules/predefiend/nixos/disko/default.nix -o /tmp/disko.nix
+  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device "\"$device\""
+  echo "--------------------"
+  echo "Disko Formate Done!"
+  
+  echo "---------------------"
+  echo "### Install System ###"
+
+  nix-shell -p git
+  read -p "Enter your username: " username
+  mkdir -p /mnt/persist/home/"$username"/flake
+  git clone https://www.github.com/akibahmed229/nixos /mnt/persist/home/"$username"/flake
+  cd /mnt/persist/home/"$username"/flake
+  read -p "Enter your hostname: " hostname
+  nixos-install --no-root-passwd /mnt --flake .#"$hostname"
+''
+
+
