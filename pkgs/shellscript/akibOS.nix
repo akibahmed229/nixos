9841f2b4f00b4d36bc64000a3db50b3ed4621f62
akibahmed229 <akib4418@gmail.com>
2024-03-08 02:28:05 +0600

    fix: dir not found! issue,.. & code refactor

--- pkgs/shellscript/akibOS.nix
+++ pkgs/shellscript/akibOS.nix
@@ -3,59 +3,105 @@
 # you can use this file to install nixos on your system
 # nix-shell -p git --command 'nix run github:akibahmed229/nixos#akibOS --experimental-features "nix-command flakes"'
 pkgs.writeShellScriptBin "akibOS" ''
-  #!/usr/bin/env bash
+    #!/usr/bin/env bash
 
-  # check if internet is working
-  if ! ping -c 1 google.com &> /dev/null; then # $> /dev/null is used to hide the output
-    echo "No internet connection!"
-    exit 1
-  fi
+  set -eo pipefail  # Exit script on error, treat pipe failures as errors
 
-  # format output
-  function my_format() {
-    echo
-    echo "--------------------"
-    echo "$1"
+  # Function to print formatted messages
+  function print_message() {
+      echo
+      echo "--------------------"
+      echo "$1"
   }
 
-  # store user input in function 
-  function user_input() {
-    read -p "$1: " $2
-    echo
+  # Function to prompt user for input
+  function prompt_user() {
+      read -rp "$1: " "$2"
+      echo
   }
-  user_input "Enter your username (e.g., akib)" username
-  user_input "Enter your hostname (e.g., nixos)" hostname
-  user_input "Enter your device (e.g., /dev/sda)" device
 
-  # check if user input is not empty
-  if [ -z "$username" ] || [ -z "$hostname" ] || [ -z "$device" ]; then
-    echo "Username, hostname, and device are required!"
-    exit 1
+  # Check internet connectivity
+  if ! ping -c 1 google.com &> /dev/null; then
+      print_message "No internet connection!"
+      exit 1
   fi
 
-  my_format "### Mounting Disk ###"
-  # download disko.nix and run disko
-  curl https://raw.githubusercontent.com/akibahmed229/nixos/main/modules/predefiend/nixos/disko/default.nix -o /tmp/disko.nix
-  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device "\"$device\""
-  my_format "### Disko Formate Done ###" 
- 
-  # install flake function
+  # Prompt user for required information
+  prompt_user "Enter your username (e.g., akib)" username
+  prompt_user "Enter your hostname (e.g., nixos)" hostname
+  prompt_user "Enter your device (e.g., /dev/sda)" device
+
+  # Check if user input is not empty
+  if [[ -z "$username" || -z "$hostname" || -z "$device" ]]; then
+      print_message "Username, hostname, and device are required!"
+      exit 1
+  fi
+
+  # Function to update user data in the flake.nix file
+  function update_flake_data() {
+      local file="/home/$username/flake/flake.nix"
+
+      # Ask the user if they want to change default values
+      read -rp "Do you want to change the default values (e.g., username, hostname, and device name)? (y/n): " ans
+
+      if [[ "$ans" =~ ^[Yy]$ ]]; then
+          # Change values in the flake.nix file according to user input
+          sed -i "s/akib/$username/g" "$file"
+          sed -i "s/nixos/$hostname/g" "$file"
+          sed -i "s,/dev/nvme0n1,$device,g" "$file"
+      else
+          print_message "You can change it later in flake.nix"
+      fi
+  }
+
+  # Function to generate hardware-configuration.nix
+  function generate_hardware_config() {
+      local config_dir="/home/$username/flake/hosts/desktop"
+
+      if [[ "$username" == "akib" && "$hostname" == "desktop" ]] || [[ "$username" == "akib" && "$hostname" == "virt" ]]; then
+          print_message "You can change it later in hardware-configuration.nix"
+          sleep 1
+      else
+          # Generate hardware-configuration.nix
+          nixos-generate-config --root /mnt
+          cp -r "/mnt/etc/nixos/hardware-configuration.nix" "$config_dir"
+      fi
+  }
+
+  # Function to install the flake and NixOS
   function install_flake() {
-    # create persist dir and clone flake
-    mkdir -p /home/$username/flake
-    git clone https://www.github.com/akibahmed229/nixos /home/$username/flake
-    rm -rf /home/$username/flake/flake.lock
-    nixos-install --no-root-passwd --flake /home/$username/flake#$hostname
-    mv /home/$username/flake /mnt/persist/home/$username/.config/
+      local flake_dir="/home/$username/flake"
+      local persist_dir="/mnt/persist/home/$username/.config"
+
+      # Clone flake repository
+      git clone https://www.github.com/akibahmed229/nixos "$flake_dir"
+      rm -rf "$flake_dir/flake.lock"
+
+      # Update user data and generate hardware configuration
+      update_flake_data
+      sleep 1
+      generate_hardware_config
+
+      # Install NixOS
+      nixos-install --no-root-passwd --flake "$flake_dir#$hostname"
+      mkdir -p "$persist_dir"
+      mv "$flake_dir" "$persist_dir"
   }
 
-  # if dir already exists, remove it 
-  if [ -d "/mnt/home" ]; then
-    sudo rm -rf /home
-    my_format "### Installing NixOS ###"
-    install_flake
-  else
-    my_format "### Installing NixOS ###"
-    install_flake
+  # Main script logic
+  print_message "### Mounting Disk ###"
+
+  # Download disko.nix and run disko
+  curl -sSfL https://raw.githubusercontent.com/akibahmed229/nixos/main/modules/predefiend/nixos/disko/default.nix -o /tmp/disko.nix
+  sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device "\"$device\""
+
+  print_message "### Disko Format Done ###"
+
+  # Check if /mnt/home directory exists
+  if [[ -d "/mnt/home" ]]; then
+      sudo rm -rf "/home/$username"
   fi
+
+  print_message "### Installing NixOS ###"
+  install_flake
 ''
