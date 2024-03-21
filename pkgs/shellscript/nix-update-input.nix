c48d76fd51da263b56cfcf3ddea55a08ff6ccc44
akibahmed229 <akib4418@gmail.com>
2024-03-21 14:23:26 +0600

    refactor: jq query to update flake input

--- pkgs/shellscript/nix-update-input.nix
+++ pkgs/shellscript/nix-update-input.nix
@@ -2,10 +2,7 @@
 
 # sudo nix run <path/to/flake.nix>/ # to update the input of a flake
 pkgs.writeShellScriptBin "update-input" ''
-  input=$(                                           \
-            nix flake metadata --json                        \
-            | ${pkgs.jq}/bin/jq ".locks.nodes.root.inputs | keys[]" \
-            | sed "s/\"//g"                                  \
-            | ${pkgs.fzf}/bin/fzf)
+  input=$(jq ".nodes.root.inputs | keys[]" < flake.lock | awk -F "\"" '{print $2}' \
+          | ${pkgs.fzf}/bin/fzf)
           nix flake lock --update-input $input
 ''
