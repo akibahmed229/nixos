eb78ad7fdb7ea8905c3a845753be5402d398dd77
akibahmed229 <akib4418@gmail.com>
2024-03-27 22:11:13 +0600

    modules: wallpaper changed

--- modules/custom/nixos/sddm/default.nix
+++ modules/custom/nixos/sddm/default.nix
@@ -22,11 +22,12 @@ in
               background = "/home/${user}/.cache/swww/Wallpaper";
             };
           };
-          sddm.theme = ''${self.packages.${pkgs.system}.custom_sddm.override  { imgLink = {
-          url = "https://raw.githubusercontent.com/akibahmed229/nixos/main/public/wallpaper/nixos.png"; 
-          sha256 = "sha256-QcY0x7pE8pKQy3At81/OFl+3CUAbx0K99ZHk85QLSo0=";
-        };
-      }}'';
+          sddm.theme = ''${self.packages.${pkgs.system}.custom_sddm.override  { 
+            imgLink = { 
+              url = "https://raw.githubusercontent.com/akibahmed229/nixos/main/public/wallpaper/evening-sky.png"; 
+              sha256 = "sha256-fYMzoY3un4qGOSR4DMqVUAFmGGil+wUze31rLLrjcAc=";
+            };
+          }}'';
         };
       };
     };
