3c823a17b84f90e4f3b0f2118cacfb2246436fdf
akibahmed229 <akib4418@gmail.com>
2024-03-27 21:08:13 +0600

    overlays: work need to be done inorder to pull latest version

--- overlays/chromium/default.nix
+++ overlays/chromium/default.nix
@@ -3,7 +3,7 @@
   chromium = prev.chromium.overrideAttrs {
     chromeSrc = builtins.fetchurl {
       url = "https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots";
-      sha256 = "sha256:1h7qgqinmrl4w2a1871x2qgdpibzbcss59z2ar4d10pqfkcgl9kv";
+      sha256 = "sha256:0hl6s91g6x8rwxv7sd6p2213jk4jf5xk8w1s9mwjjrxdv0nspdjq";
     };
   };
 })
