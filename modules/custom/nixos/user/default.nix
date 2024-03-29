08c75b4fee3a4fc73bb454892f74bb19ed3c0e11
akibahmed229 <akib4418@gmail.com>
2024-03-29 13:52:22 +0600

    modules: code refactor

--- modules/custom/nixos/user/default.nix
+++ modules/custom/nixos/user/default.nix
@@ -32,21 +32,26 @@ in
     # User settings
     # password can be hashed with: nix run nixpkgs#mkpasswd -- -m SHA-512 -s
     users = {
-      users.root.hashedPasswordFile = lib.mkIf (cfg.userName == "akib") config.sops.secrets."myservice/my_subdir/root_secret".path;
       defaultUserShell = checkUserFun cfg.userName;
       mutableUsers = lib.mkIf (config.users.users.${cfg.userName}.hashedPasswordFile != { }) true;
 
-      users.${cfg.userName} = {
-        isNormalUser = true;
-        hashedPasswordFile = lib.mkIf (cfg.userName == "akib") config.sops.secrets."myservice/my_subdir/my_secret".path;
-        hashedPassword = lib.mkIf (cfg.userName != "akib") "$6$udP2KZ8FM5LtH3od$m61..P7kY3ckU55LhG1oR8KgsqOj7T9uS1v4LUChRAn1tu/fkRa2fZskKVBN4iiKqJE5IwsUlUQewy1jur8z41";
-        initialPassword = lib.mkIf (config.users.users.${cfg.userName}.hashedPasswordFile != { }) "123456";
-        extraGroups = [ "networkmanager" "wheel" "systemd-journal" "docker" "video" "audio" "scanner" "libvirtd" "kvm" "disk" "input" "plugdev" "adbusers" "flatpak" "plex" ];
-        packages = with pkgs; [
-          wget
-          thunderbird
-          vlc
-        ];
+      users = {
+        # root user
+        root.hashedPasswordFile = lib.mkIf (cfg.userName == "akib") config.sops.secrets."myservice/my_subdir/root_secret".path;
+
+        # normal user
+        ${cfg.userName} = {
+          isNormalUser = true;
+          hashedPasswordFile = lib.mkIf (cfg.userName == "akib") config.sops.secrets."myservice/my_subdir/my_secret".path;
+          hashedPassword = lib.mkIf (cfg.userName != "akib") "$6$udP2KZ8FM5LtH3od$m61..P7kY3ckU55LhG1oR8KgsqOj7T9uS1v4LUChRAn1tu/fkRa2fZskKVBN4iiKqJE5IwsUlUQewy1jur8z41";
+          initialPassword = lib.mkIf (config.users.users.${cfg.userName}.hashedPasswordFile != { }) "123456";
+          extraGroups = [ "networkmanager" "wheel" "systemd-journal" "docker" "video" "audio" "scanner" "libvirtd" "kvm" "disk" "input" "plugdev" "adbusers" "flatpak" "plex" ];
+          packages = with pkgs; [
+            wget
+            thunderbird
+            vlc
+          ];
+        };
       };
     };
     security.sudo.wheelNeedsPassword = true; # User does not need to give password when using sudo.
