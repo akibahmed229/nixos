dec678f565caae5be3b641847160ba64acc1283e
akibahmed229 <akib4418@gmail.com>
2024-05-26 19:24:16 +0600

    flake.nix: substituters for hypr

--- flake.nix
+++ flake.nix
@@ -10,13 +10,13 @@
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
-      #"https://hyprland.cachix.org"
+      "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 
