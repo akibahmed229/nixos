c20bea624f71ca1caddb1a4079190dcd14ac3267
akibahmed229 <akib4418@gmail.com>
2024-06-01 02:01:46 +0600

    feat: hypr substitutes rm from system lvl config

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -114,12 +114,12 @@
 
       # the system-level substituters & trusted-public-keys
       extra-substituters = [
-        #"https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
       ];
       extra-trusted-public-keys = [
-        #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       ];
