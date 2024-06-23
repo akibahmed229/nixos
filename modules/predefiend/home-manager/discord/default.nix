61a7917d7507ca73cfa9f283c88fe050e9893975
akibahmed229 <akib4418@gmail.com>
2024-06-23 13:48:58 +0600

    modules: vesktop instend of discord

--- modules/predefiend/home-manager/discord/default.nix
+++ modules/predefiend/home-manager/discord/default.nix
@@ -1,18 +1,18 @@
 { pkgs, unstable, ... }:
 {
   home.packages = with pkgs; [
-    (pkgs.discord.override {
-      withOpenASAR = true;
-      withVencord = true;
-      # vencord = pkgs.vencord.overrideAttrs {
-      #   src = fetchFromGitHub {
-      #     owner = "Vendicated";
-      #     repo = "Vencord";
-      #     rev = "v1.7.8";
-      #     hash = "sha256-5kMBUdFupVxmlQ7NVJ7qzFoyQieDGHrFNkrzhlhEzJ0=";
-      #   };
-      # };
-    })
-    # unstable.${pkgs.system}.vesktop # for screen sharing on wayland
+    # (pkgs.discord.override {
+    #   withOpenASAR = true;
+    #   withVencord = true;
+    #   # vencord = pkgs.vencord.overrideAttrs {
+    #   #   src = fetchFromGitHub {
+    #   #     owner = "Vendicated";
+    #   #     repo = "Vencord";
+    #   #     rev = "v1.7.8";
+    #   #     hash = "sha256-5kMBUdFupVxmlQ7NVJ7qzFoyQieDGHrFNkrzhlhEzJ0=";
+    #   #   };
+    #   # };
+    # })
+    unstable.${pkgs.system}.vesktop # for screen sharing on wayland
   ];
 }
