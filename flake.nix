e97b01b59ef3a6393824b3163108c5cea9dcfc6e
akibahmed229 <akib4418@gmail.com>
2024-06-25 19:08:51 +0600

    flake.nix: modification related relative path

--- flake.nix
+++ flake.nix
@@ -92,7 +92,7 @@
     #  A customizable and extensible shell 
     ags.url = "github:Aylur/ags";
     # My custom nixvim 
-    nixvim.url = "github:akibahmed229/nixvim";
+    nixvim.url = "git+file:./pkgs/nixvim";
   };
 
   # outputs for the flake
