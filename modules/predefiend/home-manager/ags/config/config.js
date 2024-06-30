c5cfb3cc826752a85a2733eb1ee216e733cdd9cc
akibahmed229 <akib4418@gmail.com>
2024-07-01 04:18:29 +0600

    modules: formatted

--- modules/predefiend/home-manager/ags/config/config.js
+++ modules/predefiend/home-manager/ags/config/config.js
@@ -25,62 +25,62 @@ import { applauncher } from "./applauncher.js";
 // then you can simply instantiate one by calling it
 
 function Workspaces() {
-    const activeId = hyprland.active.workspace.bind("id");
-    const workspaces = hyprland.bind("workspaces").as((ws) =>
-        ws
-            .sort((a, b) => parseInt(a.id) - parseInt(b.id))
-            .map(({ id }) =>
-                Widget.Button({
-                    on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-                    child: Widget.Label(`${id}`),
-                    class_name: activeId.as((i) => `${i === id ? "focused" : ""}`),
-                }),
-            ),
-    );
+  const activeId = hyprland.active.workspace.bind("id");
+  const workspaces = hyprland.bind("workspaces").as((ws) =>
+    ws
+      .sort((a, b) => parseInt(a.id) - parseInt(b.id))
+      .map(({ id }) =>
+        Widget.Button({
+          on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+          child: Widget.Label(`${id}`),
+          class_name: activeId.as((i) => `${i === id ? "focused" : ""}`),
+        }),
+      ),
+  );
 
-    return Widget.Box({
-        class_name: "workspaces",
-        homogeneous: false,
-        spacing: 1,
-        children: workspaces,
-    });
+  return Widget.Box({
+    class_name: "workspaces",
+    homogeneous: false,
+    spacing: 1,
+    children: workspaces,
+  });
 }
 
 function ClientTitle() {
-    return Widget.Label({
-        class_name: "client-title",
-        label: hyprland.active.client.bind("title"),
-        justification: "left",
-        truncate: "end",
-        xalign: 0,
-        wrap: true,
-        maxWidthChars: 36,
-    });
+  return Widget.Label({
+    class_name: "client-title",
+    label: hyprland.active.client.bind("title"),
+    justification: "left",
+    truncate: "end",
+    xalign: 0,
+    wrap: true,
+    maxWidthChars: 36,
+  });
 }
 
 const date = Variable("", {
-    poll: [1000, 'date "+%A %I:%M %p,  %B %d."'],
+  poll: [1000, 'date "+%A %I:%M %p,  %B %d."'],
 });
 
 function Clock() {
-    return Widget.Label({
-        class_name: "clock",
-        justification: "left",
-        label: date.bind(),
-    });
+  return Widget.Label({
+    class_name: "clock",
+    justification: "left",
+    label: date.bind(),
+  });
 }
 
 const calendar = Widget.Calendar({
-    showDayNames: true,
-    showDetails: true,
-    showHeading: true,
-    showWeekNumbers: true,
-    detail: (self, y, m, d) => {
-        return `<span color="white">${y}. ${m}. ${d}.</span>`;
-    },
-    onDaySelected: ({ date: [y, m, d] }) => {
-        print(`${y}. ${m}. ${d}.`);
-    },
+  showDayNames: true,
+  showDetails: true,
+  showHeading: true,
+  showWeekNumbers: true,
+  detail: (self, y, m, d) => {
+    return `<span color="white">${y}. ${m}. ${d}.</span>`;
+  },
+  onDaySelected: ({ date: [y, m, d] }) => {
+    print(`${y}. ${m}. ${d}.`);
+  },
 });
 
 // we don't need dunst or any other notification daemon
@@ -121,226 +121,224 @@ const calendar = Widget.Calendar({
 // }
 
 const win = Widget.Window({
-    name: "mpris",
-    css: `padding: 10px; border-radius: 10px; margin-top: 50px; border: 1px solid shade(@theme_fg_color, 0.7);`,
-    anchor: ["top", "right"],
-    child: Media(),
+  name: "mpris",
+  css: `padding: 10px; border-radius: 10px; margin-top: 50px; border: 1px solid shade(@theme_fg_color, 0.7);`,
+  anchor: ["top", "right"],
+  child: Media(),
 });
 
 const Player = (player) =>
-    Widget.Button({
-        onClicked: () => {
-            if (win.visible) return win.hide();
-            else return win.show();
-        },
-        on_secondary_click: () => player.playPause(),
-        child: Widget.Label({
-            justification: "left",
-            truncate: "end",
-            wrap: true,
-            maxWidthChars: 16,
-        }).hook(player, (label) => {
-            const { track_artists, track_title } = player;
-            label.label = `${track_artists.join(", ")} - ${track_title}`;
-        }),
-    });
+  Widget.Button({
+    onClicked: () => {
+      if (win.visible) return win.hide();
+      else return win.show();
+    },
+    on_secondary_click: () => player.playPause(),
+    child: Widget.Label({
+      justification: "left",
+      truncate: "end",
+      wrap: true,
+      maxWidthChars: 16,
+    }).hook(player, (label) => {
+      const { track_artists, track_title } = player;
+      label.label = `${track_artists.join(", ")} - ${track_title}`;
+    }),
+  });
 
 const players = Widget.Box({
-    children: mpris.bind("players").as((p) => p.map(Player)),
+  children: mpris.bind("players").as((p) => p.map(Player)),
 });
 
 function Volume() {
-    const icons = {
-        101: "overamplified",
-        67: "high",
-        34: "medium",
-        1: "low",
-        0: "muted",
-    };
+  const icons = {
+    101: "overamplified",
+    67: "high",
+    34: "medium",
+    1: "low",
+    0: "muted",
+  };
 
-    function getIcon() {
-        const icon = audio.speaker.is_muted
-            ? 0
-            : [101, 67, 34, 1, 0].find(
-                (threshold) => threshold <= audio.speaker.volume * 100,
-            );
+  function getIcon() {
+    const icon = audio.speaker.is_muted
+      ? 0
+      : [101, 67, 34, 1, 0].find(
+          (threshold) => threshold <= audio.speaker.volume * 100,
+        );
 
-        return `audio-volume-${icons[icon]}-symbolic`;
-    }
+    return `audio-volume-${icons[icon]}-symbolic`;
+  }
 
-    const icon = Widget.Icon({
-        icon: Utils.watch(getIcon(), audio.speaker, getIcon),
-    });
+  const icon = Widget.Icon({
+    icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+  });
 
-    const slider = Widget.Slider({
-        hexpand: true,
-        draw_value: false,
-        on_change: ({ value }) => (audio.speaker.volume = value),
-        setup: (self) =>
-            self.hook(audio.speaker, () => {
-                self.value = audio.speaker.volume || 0;
-            }),
-    });
+  const slider = Widget.Slider({
+    hexpand: true,
+    draw_value: false,
+    on_change: ({ value }) => (audio.speaker.volume = value),
+    setup: (self) =>
+      self.hook(audio.speaker, () => {
+        self.value = audio.speaker.volume || 0;
+      }),
+  });
 
-    return Widget.Box({
-        class_name: "volume",
-        css: "min-width: 160px",
-        children: [icon, slider],
-    });
+  return Widget.Box({
+    class_name: "volume",
+    css: "min-width: 160px",
+    children: [icon, slider],
+  });
 }
 
 function BatteryLabel() {
-    const value = battery.bind("percent").as((p) => (p > 0 ? p / 100 : 0));
-    const icon = battery
-        .bind("percent")
-        .as((p) => `battery-level-${Math.floor(p / 10) * 10}-symbolic`);
+  const value = battery.bind("percent").as((p) => (p > 0 ? p / 100 : 0));
+  const icon = battery
+    .bind("percent")
+    .as((p) => `battery-level-${Math.floor(p / 10) * 10}-symbolic`);
 
-    return Widget.Box({
-        class_name: "battery",
-        visible: battery.bind("available"),
-        children: [
-            Widget.Icon({ icon }),
-            Widget.LevelBar({
-                widthRequest: 140,
-                vpack: "center",
-                value,
-            }),
-        ],
-    });
+  return Widget.Box({
+    class_name: "battery",
+    visible: battery.bind("available"),
+    children: [
+      Widget.Icon({ icon }),
+      Widget.LevelBar({
+        widthRequest: 140,
+        vpack: "center",
+        value,
+      }),
+    ],
+  });
 }
 // System section
 // System Tray
 function SysTray() {
-    const items = systemtray.bind("items").as((items) =>
-        items.map((item) =>
-            Widget.Button({
-                child: Widget.Icon({ icon: item.bind("icon") }),
-                on_primary_click: (_, event) => item.activate(event),
-                on_secondary_click: (_, event) => item.openMenu(event),
-                tooltip_markup: item.bind("tooltip_markup"),
-            }),
-        ),
-    );
+  const items = systemtray.bind("items").as((items) =>
+    items.map((item) =>
+      Widget.Button({
+        child: Widget.Icon({ icon: item.bind("icon") }),
+        on_primary_click: (_, event) => item.activate(event),
+        on_secondary_click: (_, event) => item.openMenu(event),
+        tooltip_markup: item.bind("tooltip_markup"),
+      }),
+    ),
+  );
 
-    return Widget.Box({
-        children: items,
-        spacing: 1,
-    });
+  return Widget.Box({
+    children: items,
+    spacing: 1,
+  });
 }
 // System Monitor Widget (CPU, Memory, Disk, Network) etc
 function SystemMonitor() {
-    const cpuLabel = Widget.Label({
-        label: "󰍛 : N/A%",
-        css: "color: @error_color; opacity: 0.8",
-    });
-    const memoryLabel = Widget.Label({
-        label: " : N/A MB",
-        css: "color: @warning_color; opacity: 0.8",
-    });
-    const diskLabel = Widget.Label({
-        label: " : N/A%",
-    });
-    const networkLabel = Widget.Label({
-        label: "  N/A    N/A",
-    });
-    const temperatureLabel = Widget.Label({
-        label: " : N/A°C",
-    });
-    const uptimeLabel = Widget.Label({
-        label: " : N/A",
-    });
+  const cpuLabel = Widget.Label({
+    label: "󰍛 : N/A%",
+    css: "color: @error_color; opacity: 0.8",
+  });
+  const memoryLabel = Widget.Label({
+    label: " : N/A MB",
+    css: "color: @warning_color; opacity: 0.8",
+  });
+  const diskLabel = Widget.Label({
+    label: " : N/A%",
+  });
+  const networkLabel = Widget.Label({
+    label: "  N/A    N/A",
+  });
+  const temperatureLabel = Widget.Label({
+    label: " : N/A°C",
+  });
+  const uptimeLabel = Widget.Label({
+    label: " : N/A",
+  });
 
-    const widget = Widget.Box({
-        spacing: 8,
-        children: [cpuLabel, memoryLabel],
-    });
+  const widget = Widget.Box({
+    spacing: 8,
+    children: [cpuLabel, memoryLabel],
+  });
 
-    Utils.interval(1000, () => {
-        const cpu = Utils.exec(`bash -c "top -bn1 | awk '/Cpu/ { print $2}'"`);
-        const memory = Utils.exec(`bash -c "free -m | awk '/Mem/{print $3}'"`);
-        const disk = Utils.exec(`bash -c "df -h | awk '/nvme1n1p2/ {print $5}'"`);
-        const network = Utils.exec(
-            `bash -c "cat /proc/net/dev | grep enp4s0 | awk '{print $2, $10}'"`,
-        );
-        const temperature = Utils.exec(
-            `bash -c "sensors | grep Package | awk '{print $4}'"`,
-        );
-        const uptime = Utils.exec(`bash -c "uptime -p"`);
+  Utils.interval(1000, () => {
+    const cpu = Utils.exec(`bash -c "top -bn1 | awk '/Cpu/ { print $2}'"`);
+    const memory = Utils.exec(`bash -c "free -m | awk '/Mem/{print $3}'"`);
+    const disk = Utils.exec(`bash -c "df -h | awk '/nvme1n1p2/ {print $5}'"`);
+    const network = Utils.exec(
+      `bash -c "cat /proc/net/dev | grep enp4s0 | awk '{print $2, $10}'"`,
+    );
+    const temperature = Utils.exec(
+      `bash -c "sensors | grep Package | awk '{print $4}'"`,
+    );
+    const uptime = Utils.exec(`bash -c "uptime -p"`);
 
-        // Update the labels with the new values
-        cpuLabel.label = `󰍛 : ${parseInt(cpu)}%`;
-        memoryLabel.label = ` : ${parseInt(memory)} MB`;
-    });
+    // Update the labels with the new values
+    cpuLabel.label = `󰍛 : ${parseInt(cpu)}%`;
+    memoryLabel.label = ` : ${parseInt(memory)} MB`;
+  });
 
-    return widget;
+  return widget;
 }
 
 function PowerMenu() {
-    return Widget.Button({
-        child: Widget.Icon({ icon: "system-shutdown-symbolic" }),
-        on_primary_click: () => Utils.exec(`bash -c "wlogout"`),
-    });
+  return Widget.Button({
+    child: Widget.Icon({ icon: "system-shutdown-symbolic" }),
+    on_primary_click: () => Utils.exec(`bash -c "wlogout"`),
+  });
 }
 
 // layout of the bar
 function Left() {
-    return Widget.Box({
-        spacing: 8,
-        children: [Workspaces(), ClientTitle()],
-    });
+  return Widget.Box({
+    spacing: 8,
+    children: [Workspaces(), ClientTitle()],
+  });
 }
 function Center() {
-    return Widget.Box({
-        spacing: 8,
-        children: [
-            Clock(),
-        ],
-    });
+  return Widget.Box({
+    spacing: 8,
+    children: [Clock()],
+  });
 }
 function Right() {
-    return Widget.Box({
-        hpack: "end",
-        spacing: 6,
-        children: [
-            Player(mpris.players[0]),
-            // Media(),
-            // Notification()
-            Volume(),
-            BatteryLabel(),
-            SystemMonitor(),
-            SysTray(),
-            PowerMenu(),
-        ],
-    });
+  return Widget.Box({
+    hpack: "end",
+    spacing: 6,
+    children: [
+      Player(mpris.players[0]),
+      // Media(),
+      // Notification()
+      Volume(),
+      BatteryLabel(),
+      SystemMonitor(),
+      SysTray(),
+      PowerMenu(),
+    ],
+  });
 }
 function Bar(monitor = 0) {
-    return Widget.Window({
-        name: `bar-${monitor}`, // name has to be unique
-        class_name: "bar",
-        monitor,
-        anchor: ["top", "left", "right"],
-        margins: [6, 8],
-        exclusivity: "exclusive",
-        child: Widget.CenterBox({
-            start_widget: Left(),
-            center_widget: Center(),
-            end_widget: Right(),
-        }),
-    });
+  return Widget.Window({
+    name: `bar-${monitor}`, // name has to be unique
+    class_name: "bar",
+    monitor,
+    anchor: ["top", "left", "right"],
+    margins: [6, 8],
+    exclusivity: "exclusive",
+    child: Widget.CenterBox({
+      start_widget: Left(),
+      center_widget: Center(),
+      end_widget: Right(),
+    }),
+  });
 }
 
 // finally, we configure the app
 App.config({
-    style: "./style.css",
-    windows: [
-        Bar(),
-        NotificationPopups(),
-        applauncher,
+  style: "./style.css",
+  windows: [
+    Bar(),
+    NotificationPopups(),
+    applauncher,
 
-        // you can call it, for each monitor
-        // Bar(0),
-        // Bar(1)
-    ],
+    // you can call it, for each monitor
+    // Bar(0),
+    // Bar(1)
+  ],
 });
 
-export { };
+export {};
