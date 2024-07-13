6ffa2785eb49546b3bbe04af81bd317b50d0815c
akibahmed229 <akib4418@gmail.com>
2024-07-13 23:23:08 +0600

    nmodules: refactor

--- modules/predefiend/nixos/networking/default.nix
+++ modules/predefiend/nixos/networking/default.nix
@@ -15,7 +15,7 @@
     nameservers = ["192.168.0.1"]; # List of DNS nameservers to use.
 
     # Set the domain name and search domain for the system.
-    domain = "${user}"; # The domain name for the system, default is "example.com".
+    domain = "${user}";
     search = ["${user}"]; # The search domain, used to resolve short hostnames.
 
     # Configure the network interfaces.
@@ -27,42 +27,39 @@
           prefixLength = 24; # Network prefix length (subnet mask).
         }
       ];
-
       # Configuration for the wireless interface 'wlp0s20f0u4'.
       wlp0s20f0u4.ipv4.addresses = [
         {
-          address = "192.168.0.179"; # Static IP address for the interface.
-          prefixLength = 24; # Network prefix length (subnet mask).
+          address = "192.168.0.179";
+          prefixLength = 24;
         }
       ];
 
-      # Configuration for the virtual interface 'vlan2'.
+      # Configuration for the VLAN interfaces.
       vlan2.ipv4.addresses = [
         {
-          address = "192.168.2.1"; # Static IP address for the interface.
-          prefixLength = 24; # Network prefix length (subnet mask).
+          address = "192.168.2.1";
+          prefixLength = 24;
         }
       ];
       vlan3.ipv4.addresses = [
         {
-          address = "192.168.3.1"; # Static IP address for the interface.
-          prefixLength = 24; # Network prefix length (subnet mask).
+          address = "192.168.3.1";
+          prefixLength = 24;
         }
       ];
     };
 
-    # Configuration for VLAN interfaces.
+    # Appending vlans to the network interfaces.
     vlans = {
-      # Configuration for VLAN with ID 2.
+      # Configuration for VLAN interfaces.
       vlan2 = {
         id = 2; # VLAN ID.
         interface = "enp4s0"; # Parent interface for the VLAN.
       };
-
-      # Configuration for VLAN with ID 3.
       vlan3 = {
-        id = 3; # VLAN ID.
-        interface = "enp4s0"; # Parent interface for the VLAN.
+        id = 3;
+        interface = "enp4s0";
       };
     };
 
@@ -70,8 +67,8 @@
     wg-quick.interfaces = {
       # Define the WireGuard interface wg0.
       wg0 = {
-        address = ["10.2.0.2/32"]; # IP address for the wg0 interface.
-        dns = ["10.2.0.1"]; # DNS server for the wg0 interface.
+        address = ["10.2.0.2/32"]; #
+        dns = ["10.2.0.1"];
 
         # Path to the private key file, managed by sops for secret management.
         privateKeyFile = config.sops.secrets."akib/wireguard/PrivateKey".path;
@@ -79,9 +76,9 @@
         # Configuration for the WireGuard peers.
         peers = [
           {
-            publicKey = "URpeZxtyUXAUUA0MWcFdCSa7F+MvchJ4eNSJUetKEGk="; # Public key of the peer.
-            allowedIPs = ["0.0.0.0/0"]; # IPs that are allowed to communicate through the peer.
-            endpoint = "212.8.253.137:51820"; # Endpoint of the peer (IP address and port).
+            publicKey = "URpeZxtyUXAUUA0MWcFdCSa7F+MvchJ4eNSJUetKEGk=";
+            allowedIPs = ["0.0.0.0/0"];
+            endpoint = "212.8.253.137:51820";
           }
         ];
       };
