09ccdee6728a31c5e47eddca2582630f179e5882
akibahmed229 <akib4418@gmail.com>
2024-07-14 12:23:37 +0600

    fix: folding disable

--- pkgs/nixvim/config/treesitter/treesitter.nix
+++ pkgs/nixvim/config/treesitter/treesitter.nix
@@ -2,15 +2,15 @@
   plugins = {
     treesitter = {
       enable = true;
+      folding = false;
       settings = {
         highlight.enable = true;
         indent.enable = true;
         ensure_installed = ["c" "lua" "vim" "vimdoc" "query" "elixir" "heex" "javascript" "html" "nix" "python" "rust" "css" "sql" "java" "json" "typescript" "yaml" "toml" "bash"];
       };
       nixvimInjections = true;
-
+      nodejsPackage = pkgs.nodejs_22;
       languageRegister.nu = "nu";
-
       grammarPackages = with pkgs;
         vimPlugins.nvim-treesitter.passthru.allGrammars
         ++ (with tree-sitter-grammars; [tree-sitter-nu]);
