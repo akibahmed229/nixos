748fe65e528ffb374f96b9bfd62b246b603ceba0
akibahmed229 <akib4418@gmail.com>
2024-07-29 18:34:21 +0600

    feat: emacs config modify

--- modules/predefiend/home-manager/emacs/init.el
+++ modules/predefiend/home-manager/emacs/init.el
@@ -1,4 +1,17 @@
-(org-babel-load-file
- (expand-file-name
-  "README.org"
-  user-emacs-directory))
+(require 'org)
+
+;; Define the path for the Org and Elisp files
+(defvar my-org-config-file (expand-file-name "README.org" user-emacs-directory))
+(defvar my-elisp-config-file (expand-file-name "config.el" user-emacs-directory))
+
+;; Tangle the Org file to Elisp if necessary
+(defun my-tangle-config-org ()
+  "Tangle README.org to config.el."
+  (when (file-newer-than-file-p my-org-config-file my-elisp-config-file)
+    (org-babel-tangle-file my-org-config-file my-elisp-config-file)))
+
+(my-tangle-config-org)
+
+;; Load the Elisp file
+(load-file my-elisp-config-file)
+
