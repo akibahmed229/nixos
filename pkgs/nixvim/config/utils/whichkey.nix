04b0adc59d276b675aa1b1634652c82f332add9a
akibahmed229 <akib4418@gmail.com>
2024-08-09 04:19:51 +0600

    nixvim: option renamed

--- pkgs/nixvim/config/utils/whichkey.nix
+++ pkgs/nixvim/config/utils/whichkey.nix
@@ -2,34 +2,35 @@
   plugins.mini.enable = true;
   plugins.which-key = {
     enable = true;
-    ignoreMissing = false; # enable this to hide mappings for which you didn't specify a label
-    showHelp = true; # show help message on the command line when the popup is visible
-    keyLabels = {};
-    icons = {
-      breadcrumb = "»";
-      separator = "➜";
-      group = "+";
-    };
-    popupMappings = {
-      scrollDown = "<c-d>";
-      scrollUp = "<c-u>";
-    };
-    window = {
-      border = "single"; # none, single, double, shadow
-      position = "bottom"; # bottom, top
-      winblend = 0;
-    };
-    layout = {
-      height = {
-        min = 4;
-        max = 25;
-      }; # min and max height of the columns
-      width = {
-        min = 20;
-        max = 50;
-      }; # min and max width of the columns
-      spacing = 3; # spacing between columns
-      align = "left"; # align columns left, center or right
+    settings = {
+      show_help = true; # show help message on the command line when the popup is visible
+      popup_mappings = {
+        scrollDown = "<c-d>";
+        scrollUp = "<c-u>";
+      };
+      key_labels = {};
+      ignore_missing = false; # enable this to hide mappings for which you didn't specify a label
+      icons = {
+        breadcrumb = "»";
+        separator = "➜";
+        group = "+";
+      };
+      window = {
+        border = "single"; # none, single, double, shadow
+        winblend = 0;
+      };
+      layout = {
+        height = {
+          min = 4;
+          max = 25;
+        }; # min and max height of the columns
+        width = {
+          min = 20;
+          max = 50;
+        }; # min and max width of the columns
+        spacing = 3; # spacing between columns
+        align = "left"; # align columns left, center or right
+      };
     };
   };
 }
