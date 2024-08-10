a6037a96248f6fbeda76d5b6c50c506decbb3c71
akibahmed229 <akib4418@gmail.com>
2024-08-10 08:31:41 +0600

    flake.lock: updated

---
+++ hosts/nixos/minimalISO/default.nix
@@ -0,0 +1,72 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [
+    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+  ];
+
+  # The default compression-level is (6) and takes too long on some machines (>30m). 3 takes <2m
+  isoImage.squashfsCompression = "zstd -Xcompression-level 3";
+
+  nixpkgs = {
+    hostPlatform = lib.mkDefault "x86_64-linux";
+    config.allowUnfree = true;
+  };
+
+  # FIXME: Reference generic nix file
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  environment.systemPackages = with pkgs; [
+    # for debugging
+    htop
+    vim
+    wget
+    curl
+    git
+    # for ssh
+    openssh
+    # for nix
+    nix
+  ];
+
+  services = {
+    qemuGuest.enable = true;
+    openssh = {
+      ports = [8080];
+      settings.PermitRootLogin = lib.mkForce "yes";
+    };
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = lib.mkForce [
+      "btrfs"
+      "vfat"
+    ];
+  };
+
+  networking = {
+    hostName = "iso";
+  };
+
+  systemd = {
+    services.sshd.wantedBy = lib.mkForce ["multi-user.target"];
+    # gnome power settings to not turn off screen
+    targets = {
+      sleep.enable = false;
+      suspend.enable = false;
+      hibernate.enable = false;
+      hybrid-sleep.enable = false;
+    };
+  };
+}
