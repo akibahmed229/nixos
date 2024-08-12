10f5ab674552d30108be3ad77cbb6f2cf33c40ca
akibahmed229 <akib4418@gmail.com>
2024-08-12 15:02:34 +0600

    host home: unfree packages enabled

--- hosts/homeManager/arch/default.nix
+++ hosts/homeManager/arch/default.nix
@@ -17,6 +17,11 @@ in {
     overlays = [
       self.overlays.nvim-overlay
     ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
   };
 
   # imports from the predefiend modules folder
