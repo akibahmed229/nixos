1dd0c089606017768d1f77cc911cbf2110753a52
akibahmed229 <akib4418@gmail.com>
2024-08-16 02:28:46 +0600

    overlays: disable amd kernel config

--- overlays/intel-kernel/default.nix
+++ overlays/intel-kernel/default.nix
@@ -8,13 +8,27 @@
       kernel = super.kernel.override {
         # Custom kernel configuration
         structuredExtraConfig = with inputs.nixpkgs.lib.kernel; {
-          SCHED_MUQSS = yes;
+          # enable Intel-specific options
           CONFIG_INTEL_PMC_CORE = yes;
           CONFIG_PERF_EVENTS_INTEL_UNCORE = yes;
           CONFIG_DRM_I915 = yes;
           CONFIG_DRM_I915_GVT_KVMGT = yes;
           CONFIG_INTEL_POWERCLAMP = yes;
           CONFIG_X86_ACPI_CPUFREQ = yes;
+
+          # disable AMD-specific options
+          CONFIG_AMD_NB = option no;
+          CONFIG_X86_AMD_PLATFORM_DEVICE = option no;
+          CONFIG_CPU_SUP_AMD = option no;
+          CONFIG_DRM_AMDGPU = option no;
+          CONFIG_AMD_IOMMU = option no;
+          CONFIG_AMD_XGBE = option no;
+          CONFIG_FB_RADEON = option no;
+          CONFIG_DRM_RADEON = option no;
+          CONFIG_X86_AMD_PSTATE = option no;
+          CONFIG_KVM_AMD = option no;
+          CONFIG_HW_RANDOM_AMD = option no;
+          CONFIG_AGP_AMD64 = option no;
         };
         # Add custom kernel patches from the patches directory
         kernelPatches = [
