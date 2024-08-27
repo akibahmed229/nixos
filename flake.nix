b7fbfd9bd8f06ed31778535b1d7d63476f27ba76
akibahmed229 <akib4418@gmail.com>
2024-08-27 15:31:29 +0600

    feat: changes related github actions

--- flake.nix
+++ flake.nix
@@ -123,7 +123,7 @@
     };
     # Private secrets repo. Authenticate via ssh and use shallow clone
     mySsecrets = {
-      url = "git+ssh://git@gitlab.com/akibahmed/sops-secrects.git?ref=main&shallow=1&allRefs=true";
+      url = "git+ssh://git@gitlab.com/akibahmed/sops-secrects.git";
       flake = false;
     };
   };
