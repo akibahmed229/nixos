c11b714fc468f2e0a32a170ce113b11ce8e8242a
akibahmed229 <akib4418@gmail.com>
2024-09-04 18:34:23 +0600

    pkgs: condition added to which theme use

--- pkgs/custom_sddm/default.nix
+++ pkgs/custom_sddm/default.nix
@@ -4,6 +4,7 @@
     url = "https://raw.githubusercontent.com/akibahmed229/nixos/main/public/wallpaper/Photo-of-Valley.jpg";
     sha256 = "sha256-86ja7yfSdMAaFFdfDqQS8Yq5yjZjXPMLTIgExcnzbXc=";
   },
+  sugar-dark ? false,
   ...
 }: let
   isValidUrl = url: builtins.match ''(https?|ftp)://[^\s/$.?#].*'' url != null;
@@ -19,34 +20,48 @@
     else
       # what should be assigned to a
       throw "Invalid URL. Please provide a valid URL.";
+
+  src =
+    if sugar-dark
+    then
+      pkgs.fetchFromGitHub {
+        owner = "MarianArlt";
+        repo = "sddm-sugar-dark";
+        rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
+        sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+      }
+    else
+      pkgs.fetchFromGitHub {
+        owner = "gpskwlkr";
+        repo = "sddm-astronaut-theme";
+        rev = "468a100460d5feaa701c2215c737b55789cba0fc";
+        sha256 = "1h20b7n6a4pbqnrj22y8v5gc01zxs58lck3bipmgkpyp52ip3vig";
+      };
 in
   pkgs.stdenv.mkDerivation {
     name = "sddm-theme";
-    #src = pkgs.fetchFromGitHub {
-    #  owner = "MarianArlt";
-    #  repo = "sddm-sugar-dark";
-    #  rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-    #  sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
-    #};
-
-    src = pkgs.fetchFromGitHub {
-      owner = "gpskwlkr";
-      repo = "sddm-astronaut-theme";
-      rev = "468a100460d5feaa701c2215c737b55789cba0fc";
-      sha256 = "1h20b7n6a4pbqnrj22y8v5gc01zxs58lck3bipmgkpyp52ip3vig";
-    };
+    inherit src;
 
     installPhase = ''
       mkdir -p $out
       cp -R ./* $out/
       cd $out/
-      rm Backgrounds/background.png
-      cp -r ${image} $out/Backgrounds/background.png
+
+      if [ "${toString sugar-dark}" = "true" ]; then
+        rm Background.jpg
+        cp -r ${image} $out/Background.jpg
+      else
+        rm Backgrounds/background.png
+        cp -r ${image} $out/Backgrounds/background.png
+      fi
     '';
 
     meta = with pkgs.lib; {
-      description = "A dark theme for SDDM";
-      homepage = "https://github.com/MarianArlt/sddm-sugar-dark";
+      description = "Theme for SDDM";
+      homepage = [
+        "https://github.com/MarianArlt/sddm-sugar-dark"
+        "https://github.com/gpskwlkr/sddm-astronaut-theme"
+      ];
       platforms = platforms.linux;
       maintainers = [maintainers.akibahmed229];
       license = licenses.gpl3;
