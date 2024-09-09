893c6689b52197a297173df63e1ef1f7c2642c73
akibahmed229 <akib4418@gmail.com>
2024-09-10 00:13:40 +0600

    feat: plotly pkgs added in jupyter

--- devshells/shell/python/jupyter.nix
+++ devshells/shell/python/jupyter.nix
@@ -10,26 +10,8 @@ pkgs.mkShell {
   nativeBuildInputs =
     (with pkgs; [
       pkgs.stdenv.cc.cc.lib
-
       git-crypt
       stdenv.cc.cc # jupyter lab needs
-
-      python3
-      python311Packages.pip
-      python311Packages.ipykernel
-      python311Packages.jupyterlab
-      python311Packages.pyzmq
-      python311Packages.venvShellHook
-      python311Packages.pip
-      python311Packages.numpy
-      python311Packages.pandas
-      python311Packages.pandas-datareader
-      python311Packages.matplotlib
-      python311Packages.requests
-      python311Packages.seaborn
-      python311Packages.openpyxl
-      # python311Packages.sklearn-deap
-
       # sometimes you might need something additional like the following - you will get some useful error if it is looking for a binary in the environment.
       taglib
       openssl
@@ -39,8 +21,24 @@ pkgs.mkShell {
       libzip
       zlib
     ])
-    ++ (with unstable; [
-      python311Packages.virtualenv
+    ++ (with unstable.python311Packages; [
+      virtualenv
+      pip
+      ipykernel
+      jupyterlab
+      pyzmq
+      venvShellHook
+      pip
+      numpy
+      pandas
+      pandas-datareader
+      matplotlib
+      requests
+      seaborn
+      openpyxl
+      cufflinks
+      plotly
+      # sklearn-deap
     ]);
 
   # Workaround: make vscode's python extension read the .venv
