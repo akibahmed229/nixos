9924302c22f8f770c8ea879ae3245d940a1ff000
akibahmed229 <akib4418@gmail.com>
2024-10-19 20:22:36 +0600

    feat: pkgs steam runtime removed

--- modules/predefiend/nixos/gaming/default.nix
+++ modules/predefiend/nixos/gaming/default.nix
@@ -11,9 +11,8 @@
     steam = {
       enable = true;
       package = pkgs.steam;
-      extraPackages = with pkgs.steamPackages; [
-        steam-runtime
-      ];
+      # extraPackages = with pkgs.steamPackages; [
+      # ];
       # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       gamescopeSession.enable = true; # optimize micro compositor for games
