4c379ece44aa4f560a9f3814dfa62d1e49f89954
akibahmed229 <akib4418@gmail.com>
2024-11-12 10:48:24 +0600

    module: specialisation added for different requirement

---
+++ modules/predefiend/nixos/specialisation/default.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  self,
+  lib,
+  config,
+  ...
+}: let
+  # My custom lib helper functions
+  inherit (self.lib) mkImport mkRelativeToRoot;
+  path = mkRelativeToRoot "modules/predefiend/nixos";
+in {
+  specialisation = {
+    "DevOPS".configuration = {
+      imports = mkImport {
+        inherit path;
+        ListOfPrograms = ["kubernetes"];
+      };
+    };
+
+    "Gaming".configuration = {
+      imports = mkImport {
+        inherit path;
+        ListOfPrograms = ["gaming"];
+      };
+    };
+  };
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "switch-spec" ''
+      if [ $# -ne 1 ]; then
+        echo "Usage: switch-spec <specialisation>"
+        exit 1
+      fi
+
+      sudo /nix/var/nix/profiles/system/specialisation/$1/bin/switch-to-configuration switch
+    '')
+  ];
+  environment.sessionVariables = lib.mkIf (config.specialisation != {}) {
+    SPECIALISATION = "NONE";
+  };
+}
