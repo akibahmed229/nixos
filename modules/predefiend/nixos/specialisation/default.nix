b1f0b31c447b8ef9354c8b36a61da522146d318e
akibahmed229 <akib4418@gmail.com>
2024-11-12 10:56:38 +0600

    module: specialisation added for different system requirement

--- modules/predefiend/nixos/specialisation/default.nix
+++ modules/predefiend/nixos/specialisation/default.nix
@@ -9,6 +9,10 @@
   inherit (self.lib) mkImport mkRelativeToRoot;
   path = mkRelativeToRoot "modules/predefiend/nixos";
 in {
+  /*
+  Specialisation is a way to configure your system based on your needs.
+  It will Create boot entry for each specialisation and will allow you to switch between them.
+  */
   specialisation = {
     "DevOPS".configuration = {
       imports = mkImport {
@@ -35,6 +39,7 @@ in {
       sudo /nix/var/nix/profiles/system/specialisation/$1/bin/switch-to-configuration switch
     '')
   ];
+
   environment.sessionVariables = lib.mkIf (config.specialisation != {}) {
     SPECIALISATION = "NONE";
   };
