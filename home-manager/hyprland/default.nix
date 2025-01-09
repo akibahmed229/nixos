a671a5e22f9f6bf61b793d50018e76296c950f35
akibahmed229 <akib4418@gmail.com>
2025-01-10 00:11:42 +0600

    feat: based on hyprland new ver, changes aapplied

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -18,8 +18,11 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+    # set the flake package
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     xwayland.enable = true;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   environment = {
