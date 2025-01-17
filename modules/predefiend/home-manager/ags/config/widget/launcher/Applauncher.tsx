9c463c9af32279b6c8632c8fe5717202eb725362
akibahmed229 <akib4418@gmail.com>
2025-01-18 01:50:12 +0600

    feat: ags v2 applauncher rewrite

--- modules/predefiend/home-manager/ags/config/widget/launcher/Applauncher.tsx
+++ modules/predefiend/home-manager/ags/config/widget/launcher/Applauncher.tsx
@@ -1,90 +1,199 @@
 import Apps from "gi://AstalApps";
 import { App, Astal, Gdk, Gtk } from "astal/gtk3";
-import { Variable } from "astal";
+import { Variable, bind } from "astal";
+import { execAsync } from "astal/process";
 
-const MAX_ITEMS = 8;
+export const AppLauncherWindowName = "launcher";
 
 function hide() {
-  App.get_window("launcher")!.hide();
+  App.get_window(AppLauncherWindowName)!.hide();
 }
 
-function AppButton({ app }: { app: Apps.Application }) {
+function launchApp(app: Apps.Application) {
+  execAsync(`uwsm app -- ${app.entry}`).catch((error) => {
+    print(error);
+  });
+}
+
+interface AppButtonProps {
+  app: Apps.Application;
+  isSelected: boolean;
+  indexInList: number;
+  selectedIndexVariable: Variable<number>;
+}
+
+function AppButton({
+  app,
+  isSelected,
+  indexInList,
+  selectedIndexVariable,
+}: AppButtonProps) {
   return (
     <button
-      className="AppButton"
+      canFocus={false}
+      className={isSelected ? "selectedAppButton" : "appButton"}
+      onHover={() => {
+        // Uncomment to select the hovered app.
+        // selectedIndexVariable.set(indexInList)
+      }}
       onClicked={() => {
         hide();
-        app.launch();
+        launchApp(app);
       }}
     >
       <box>
         <icon icon={app.iconName} />
-        <box valign={Gtk.Align.CENTER} vertical>
+        <box valign={Gtk.Align.CENTER} vertical={true}>
           <label className="name" truncate xalign={0} label={app.name} />
-          {app.description && (
-            <label
-              className="description"
-              wrap
-              xalign={0}
-              label={app.description}
-            />
-          )}
         </box>
       </box>
     </button>
   );
 }
 
-export default function Applauncher() {
+export default function AppLauncher() {
   const { CENTER } = Gtk.Align;
   const apps = new Apps.Apps();
 
+  const selectedIndex = Variable(0);
   const text = Variable("");
-  const list = text((text) => apps.fuzzy_query(text).slice(0, MAX_ITEMS));
+  const list = text((text) => {
+    let listApps = apps
+      .exact_query(text)
+      .filter((app) => app.name.toLowerCase().includes(text.toLowerCase()))
+      .sort((a, b) => {
+        if (a.name === b.name) {
+          return 0;
+        }
+        let aMatch = a.name.toLowerCase().startsWith(text.toLowerCase());
+        let bMatch = b.name.toLowerCase().startsWith(text.toLowerCase());
+        if (aMatch && bMatch) {
+          if (a.name > b.name) {
+            return 1;
+          } else {
+            return -1;
+          }
+        } else if (aMatch) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    if (listApps.length - 1 < selectedIndex.get()) {
+      if (listApps.length === 0) {
+        selectedIndex.set(0);
+      } else {
+        selectedIndex.set(listApps.length - 1);
+      }
+    }
+    return listApps;
+  });
   const onEnter = () => {
-    apps.fuzzy_query(text.get())?.[0].launch();
+    if (list.get().length > 0) {
+      const app = list.get()?.[selectedIndex.get()];
+      if (app !== null) {
+        launchApp(app);
+      }
+    }
     hide();
   };
+  const listBinding = Variable.derive([list, selectedIndex]);
+
+  let window: Gtk.Window;
 
   return (
     <window
-      name="launcher"
-      anchor={Astal.WindowAnchor.CENTER}
+      name={AppLauncherWindowName}
+      anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.BOTTOM}
       exclusivity={Astal.Exclusivity.IGNORE}
       keymode={Astal.Keymode.EXCLUSIVE}
+      layer={Astal.Layer.TOP}
       application={App}
-      onShow={() => text.set("")}
+      onShow={() => {
+        text.set("");
+        selectedIndex.set(0);
+      }}
       onKeyPressEvent={function (self, event: Gdk.Event) {
-        if (event.get_keyval()[1] === Gdk.KEY_Escape) self.hide();
+        if (event.get_keyval()[1] === Gdk.KEY_Escape) {
+          self.hide();
+        } else if (
+          event.get_keyval()[1] === Gdk.KEY_Down &&
+          list.get().length >= selectedIndex.get()
+        ) {
+          selectedIndex.set(selectedIndex.get() + 1);
+        } else if (
+          event.get_keyval()[1] === Gdk.KEY_Up &&
+          selectedIndex.get() != 0
+        ) {
+          selectedIndex.set(selectedIndex.get() - 1);
+        }
+      }}
+      marginTop={100}
+      marginBottom={5}
+      visible={false}
+      setup={(self) => {
+        window = self;
       }}
     >
-      <box>
-        <eventbox widthRequest={4000} expand onClick={hide} />
-        <box hexpand={false} vertical>
-          <eventbox heightRequest={100} onClick={hide} />
-          <box widthRequest={500} className="Applauncher" vertical>
-            <entry
-              placeholderText="Search"
-              text={text()}
-              onChanged={(self) => text.set(self.text)}
-              onActivate={onEnter}
-            />
-            <box spacing={6} vertical>
-              {list.as((list) => list.map((app) => <AppButton app={app} />))}
+      <box vertical={true}>
+        <box
+          setup={(self) => {
+            setTimeout(() => {
+              bind(window, "hasToplevelFocus").subscribe((hasFocus) => {
+                if (hasFocus) {
+                  self.className = "focusedWindow";
+                } else {
+                  self.className = "window";
+                }
+              });
+            }, 1_000);
+          }}
+        >
+          <box widthRequest={500} className="appLauncher" vertical={true}>
+            <box vertical={false}>
+              <icon icon="system-search-symbolic" className="searchField" />
+              <entry
+                className="searchField"
+                placeholderText="Search"
+                text={text()}
+                onChanged={(self) => text.set(self.text)}
+                onActivate={onEnter}
+                hexpand={true}
+              />
             </box>
-            <box
-              halign={CENTER}
-              className="not-found"
-              vertical
-              visible={list.as((l) => l.length === 0)}
+            <scrollable
+              className="scrollWindow"
+              vscroll={Gtk.PolicyType.AUTOMATIC}
+              propagateNaturalHeight={true}
+              canFocus={false}
             >
-              <icon icon="system-search-symbolic" />
-              <label label="No match found" />
-            </box>
+              <box spacing={6} vertical={true}>
+                {listBinding((value) =>
+                  value[0].map((app, index) => (
+                    <AppButton
+                      app={app}
+                      isSelected={index === value[1]}
+                      indexInList={index}
+                      selectedIndexVariable={selectedIndex}
+                    />
+                  )),
+                )}
+                <box
+                  halign={CENTER}
+                  vertical={true}
+                  css={`
+                    margin-bottom: 8px;
+                  `}
+                  visible={list.as((l) => l.length === 0)}
+                >
+                  <label className="labelSmall" label="No match found" />
+                </box>
+                <box />
+              </box>
+            </scrollable>
           </box>
-          <eventbox expand onClick={hide} />
         </box>
-        <eventbox widthRequest={4000} expand onClick={hide} />
+        <box vexpand={true} />
       </box>
     </window>
   );
