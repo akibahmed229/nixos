fb9f056d65f3568399b78d1522f0607c04475ddf
akibahmed229 <akib4418@gmail.com>
2025-04-14 10:44:38 +0600

    feat: nodejsPackage option removed

--- pkgs/nixvim/config/treesitter/treesitter.nix
+++ pkgs/nixvim/config/treesitter/treesitter.nix
@@ -9,7 +9,6 @@
         ensure_installed = ["c" "lua" "vim" "vimdoc" "query" "elixir" "heex" "javascript" "html" "nix" "python" "rust" "css" "sql" "java" "json" "typescript" "yaml" "toml" "bash"];
       };
       nixvimInjections = true;
-      nodejsPackage = pkgs.nodejs_22;
       languageRegister.nu = "nu";
       grammarPackages = with pkgs;
         vimPlugins.nvim-treesitter.passthru.allGrammars
