8b1547613fec8e1642d1d85a24fce0a5ce42425c
akibahmed229 <akib4418@gmail.com>
2025-08-15 19:01:18 +0600

    fix: ollama docker container added

--- modules/predefiend/nixos/docker/container/ollama.nix
+++ modules/predefiend/nixos/docker/container/ollama.nix
@@ -3,15 +3,6 @@
     backend = "docker";
 
     containers = {
-      ollama = {
-        image = "ollama/ollama";
-        ports = ["11434:11434"];
-        volumes = [
-          "ollama:/root/.ollama"
-        ];
-        autoStart = true;
-      };
-
       ollama-webui = {
         image = "ghcr.io/open-webui/open-webui:main";
         ports = ["4801:8080"];
@@ -19,10 +10,19 @@
           OLLAMA_BASE_URL = "http://localhost:11434"; # Points to Ollama container
         };
         volumes = [
-          "ollama-webui:/app/backend/data"
+          "/var/lib/ollama-webui:/app/backend/data"
         ];
         extraOptions = [
           "--link=ollama:ollama"
+        ];
+        autoStart = true;
+      };
+
+      ollama = {
+        image = "ollama/ollama";
+        ports = ["11434:11434"];
+        volumes = [
+          "/var/lib/ollama:/root/.ollama"
         ];
         autoStart = true;
       };
