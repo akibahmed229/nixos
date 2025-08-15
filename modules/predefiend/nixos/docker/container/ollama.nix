19b03b0c24a1a55540a7ebdf804ad34be877069a
akibahmed229 <akib4418@gmail.com>
2025-08-16 01:59:01 +0600

    fix: ollama webui commented

--- modules/predefiend/nixos/docker/container/ollama.nix
+++ modules/predefiend/nixos/docker/container/ollama.nix
@@ -3,20 +3,20 @@
     backend = "docker";
 
     containers = {
-      ollama-webui = {
-        image = "ghcr.io/open-webui/open-webui:main";
-        ports = ["4801:8080"];
-        environment = {
-          OLLAMA_BASE_URL = "http://localhost:11434"; # Points to Ollama container
-        };
-        volumes = [
-          "/var/lib/ollama-webui:/app/backend/data"
-        ];
-        extraOptions = [
-          "--link=ollama:ollama"
-        ];
-        autoStart = true;
-      };
+      # ollama-webui = {
+      #   image = "ghcr.io/open-webui/open-webui:main";
+      #   ports = ["4801:8080"];
+      #   environment = {
+      #     OLLAMA_BASE_URL = "http://localhost:11434"; # Points to Ollama container
+      #   };
+      #   volumes = [
+      #     "/var/lib/ollama-webui:/app/backend/data"
+      #   ];
+      #   extraOptions = [
+      #     "--link=ollama:ollama"
+      #   ];
+      #   autoStart = true;
+      # };
 
       ollama = {
         image = "ollama/ollama";
