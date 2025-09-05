666b45f08f4cdd4af22cedb024819e5147d17338
akibahmed229 <akib4418@gmail.com>
2025-09-06 00:50:13 +0600

    chore(hosts): sops added in virt

--- hosts/nixos/virt/home.nix
+++ hosts/nixos/virt/home.nix
@@ -5,6 +5,6 @@
 in {
   imports = mkImport {
     path = mkRelativeToRoot "modules/predefiend/home-manager";
-    ListOfPrograms = ["firefox" "zsh" "tmux" "lf" "git"];
+    ListOfPrograms = ["firefox" "zsh" "tmux" "lf" "git" "sops"];
   };
 }
