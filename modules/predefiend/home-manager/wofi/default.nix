84980d8d29d6be51ba120bd33b0264d084cd7778
akibahmed229 <akib4418@gmail.com>
2025-09-06 18:36:43 +0600

    chore(modules): wof config related changes

--- modules/predefiend/home-manager/wofi/default.nix
+++ modules/predefiend/home-manager/wofi/default.nix
@@ -1,10 +1,22 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.wofi = {
     enable = true;
     package = pkgs.wofi;
     settings = {
-      mode = "drun";
       allow_images = true;
+      show = "drun";
+      prompt = "Search";
+      term = "kitty";
+      print_command = true;
+      insensitive = true;
+      columns = 1;
+      no_actions = true;
+      hide_scroll = true;
+      style = "${config.home.homeDirectory}/.config/wofi/style.css";
     };
   };
 }
