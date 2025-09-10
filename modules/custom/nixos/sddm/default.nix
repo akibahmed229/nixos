173324d9976ee02103ffa0d0b39bff7e889cc0e1
akibahmed229 <akib4418@gmail.com>
2025-09-11 00:21:43 +0600

    feat(modules): default session option commented

--- modules/custom/nixos/sddm/default.nix
+++ modules/custom/nixos/sddm/default.nix
@@ -24,7 +24,7 @@ in {
     services = {
       libinput.enable = true;
       displayManager = {
-        defaultSession = "hyprland-uwsm";
+        # defaultSession = "hyprland-uwsm";
         sddm = {
           enable = true;
           wayland = {
