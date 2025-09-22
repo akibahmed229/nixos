2bb37403785287bc74ea575310052464294ab589
akibahmed229 <akib4418@gmail.com>
2025-09-22 15:35:52 +0600

    feat(host virt): nixvim replaced with raw nvim config

--- hosts/nixos/virt/home.nix
+++ hosts/nixos/virt/home.nix
@@ -1,20 +1,21 @@
 # virt home-manager configuration
-{
-  pkgs,
-  self,
-  lib,
-  user,
-  ...
-}: let
+{ pkgs
+, self
+, lib
+, user
+, ...
+}:
+let
   # My custom lib helper functions
   inherit (self.lib) mkImport mkRelativeToRoot;
-in {
+in
+{
   imports = mkImport {
     path = mkRelativeToRoot "modules/predefiend/home-manager";
     ListOfPrograms =
       [
         "firefox"
-        "nixvim"
+        "nvim"
         "thunar"
       ]
       ++ lib.optionals (user == "akib") [
