ecb7ca8f8d4512b0ff61f30337e08266ed512d00
akibahmed229 <akib4418@gmail.com>
2025-10-10 23:15:02 +0600

    fix (virt): multiple password option

--- hosts/nixos/x86_64-linux/virt/users/main/default.nix
+++ hosts/nixos/x86_64-linux/virt/users/main/default.nix
@@ -18,7 +18,10 @@
     else null;
 
   # Optional fallback password (hashed) if secret not used
-  hashedPassword = "$6$udP2KZ8FM5LtH3od$m61..P7kY3ckU55LhG1oR8KgsqOj7T9uS1v4LUChRAn1tu/fkRa2fZskKVBN4iiKqJE5IwsUlUQewy1jur8z41";
+  hashedPassword =
+    if !(config ? sops.secrets."${name}/password/my_secret")
+    then "$6$udP2KZ8FM5LtH3od$m61..P7kY3ckU55LhG1oR8KgsqOj7T9uS1v4LUChRAn1tu/fkRa2fZskKVBN4iiKqJE5IwsUlUQewy1jur8z41"
+    else null;
 
   # SSH or GPG keys (extend as needed)
   openssh.authorizedKeys.keys = [];
