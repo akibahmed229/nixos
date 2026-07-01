3d3dcff10a04ec1bd9dd13e6e8ec6cf7099f373c
akibahmed229 <akib4418@gmail.com>
2026-07-01 15:00:46 +0600

    feat (common): permitted insecure pkgs for font config

--- hosts/nixos/x86_64-linux/home.nix
+++ hosts/nixos/x86_64-linux/home.nix
@@ -14,7 +14,10 @@ in {
   ];
 
   nixpkgs = {
-    config.allowUnfree = true;
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = ["pnpm-10.29.2"];
+    };
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
