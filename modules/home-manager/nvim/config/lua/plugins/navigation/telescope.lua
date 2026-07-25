143dee5a9247315c0f17cb6fa223f5546829d60c
akibahmed229 <akib4418@gmail.com>
2026-07-25 09:58:22 +0600

    feat (hm-module nvim): treesitter, telescope option related changes

--- modules/home-manager/nvim/config/lua/plugins/navigation/telescope.lua
+++ modules/home-manager/nvim/config/lua/plugins/navigation/telescope.lua
@@ -1,5 +1,5 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local actions = require('telescope.actions')
