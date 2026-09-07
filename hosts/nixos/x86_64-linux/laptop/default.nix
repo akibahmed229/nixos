cbd00a48ad3cc1faaf482024dd86b803572e2404
akibahmed229 <akib4418@gmail.com>
2026-09-08 00:36:10 +0600

    feat (host): wsl added for laptop

--- hosts/nixos/x86_64-linux/laptop/default.nix
+++ hosts/nixos/x86_64-linux/laptop/default.nix
@@ -12,9 +12,13 @@
     inputs.nixos-wsl.nixosModules.default
   ];
 
+  # Basic WSL Configuration
+  wsl.enable = true;
+  wsl.defaultUser = user; # Uses "akib" from your flake.nix
+
+  # Optional but recommended for WSL:
+  wsl.startMenuLaunchers = true;
+
   # Enable Nix Flakes in the WSL instance
   nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # System state version (you can also map this to your global state-version if preferred)
-  system.stateVersion = "24.05";
 }
--- hosts/nixos/x86_64-linux/laptop/hardware-configuration.nix
+++ hosts/nixos/x86_64-linux/laptop/hardware-configuration.nix
@@ -1,14 +1,2 @@
-{
-  inputs,
-  lib,
-  pkgs,
-  user,
-  ...
-}: {
-  # Basic WSL Configuration
-  wsl.enable = true;
-  wsl.defaultUser = user; # Uses "akib" from your flake.nix
-
-  # Optional but recommended for WSL:
-  wsl.startMenuLaunchers = true;
+{pkgs, ...}: {
 }
